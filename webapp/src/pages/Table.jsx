151f6c38eedb347584bdbdd57e20ea09873d99dd
Simon Mukunga <mmukunga@gmail.com>
2021-01-10 00:51:57 +0100

    first commit

--- webapp/src/pages/Table.jsx
+++ webapp/src/pages/Table.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 function Table({ children, ...props }) {
-    const firstRow = []||props.data[0];
+    const firstRow = props.data[0];
     console.log('1.firstRow..');
     console.log(firstRow);
     console.log('2.firstRow..');
