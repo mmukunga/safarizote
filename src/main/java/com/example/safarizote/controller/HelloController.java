4770683b6f4af06f8f35ad53a43a1733471f3ede
Simon Mukunga <mmukunga@gmail.com>
2021-01-10 22:20:14 +0100

    first commit

--- src/main/java/com/example/safarizote/controller/HelloController.java
+++ src/main/java/com/example/safarizote/controller/HelloController.java
@@ -2,9 +2,11 @@ package com.example.safarizote.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.safarizote.model.Tracker;
