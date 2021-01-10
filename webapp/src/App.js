e7b4b8a11076d7cd098535c1f74f7594ed77b509
Simon Mukunga <mmukunga@gmail.com>
2021-01-10 23:53:41 +0100

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -35,7 +35,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Samuel
         </a>
       </header>
       <Router>
