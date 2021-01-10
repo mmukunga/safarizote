d5be3df421c403da3a325a56fc94275af19281e6
Simon Mukunga <mmukunga@gmail.com>
2021-01-10 23:18:05 +0100

    first commit

--- src/main/java/com/example/safarizote/controller/HelloController.java
+++ src/main/java/com/example/safarizote/controller/HelloController.java
@@ -7,6 +7,7 @@ import java.time.Instant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
