9ca04a956e9b105863e4d6b52716bd620d9e20ed
Simon Mukunga <mmukunga@gmail.com>
2021-01-11 13:31:40 +0100

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 
 import logo from './logo.svg';
 import './App.css';
