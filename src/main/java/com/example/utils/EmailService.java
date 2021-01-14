6289695128ca6fb739a1b5e16e62eeb30bbe72b8
Simon Mukunga <mmukunga@gmail.com>
2021-01-14 23:40:40 +0100

    first commit

--- src/main/java/com/example/utils/EmailService.java
+++ src/main/java/com/example/utils/EmailService.java
@@ -1,18 +1,14 @@
 package com.example.utils;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 import java.io.IOException;
-import java.util.Properties;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
