c3197ff9631fabb778a281c838b84bdd801d776a
Simon Mukunga <mmukunga@gmail.com>
2021-01-16 21:14:24 +0100

    first commit

--- src/main/java/com/example/safarizote/repository/CountryLoader.java
+++ src/main/java/com/example/safarizote/repository/CountryLoader.java
@@ -37,35 +37,49 @@ public class CountryLoader implements CommandLineRunner {
 */
         if (repository.count() == 0) {
             String fileName = "countries.json";
+            System.out.println("Countries..1");  
             ClassLoader classLoader = getClass().getClassLoader();
+            System.out.println("Countries..2");  
             try (InputStream is = classLoader.getResourceAsStream(fileName);
             Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
                 Gson gson = new Gson();
+                System.out.println("Countries..3");  
                 Country[] countries = gson.fromJson(reader, Country[].class);   
                 Arrays.stream(countries).forEach( e -> {
                     List<City> cities = new ArrayList<>();
+                    System.out.println("Countries..4");  
                     repository.save(Country.builder()
                     .name(e.getName()).code(e.getCode())
                     .cities(cities).build());                
                     System.out.println(e);  
                 });
             } 
+        } else {
+            System.out.println("Countries..NOT EMPTY!!!");  
         }
         
+        System.out.println("Cities..SIZE!!!" + cityRepository.count()); 
+         
         if (cityRepository.count() == 0) {
             String fileCityName = "cities.json";
+            System.out.println("Cities..1");  
             ClassLoader classCityLoader = getClass().getClassLoader();
+            System.out.println("Cities..2"); 
             try (InputStream is = classCityLoader.getResourceAsStream(fileCityName);
             Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
                 Gson gson = new Gson();
                 City[] cities = gson.fromJson(reader, City[].class);   
+                System.out.println("Cities..3"); 
                 Arrays.stream(cities).forEach( city -> {
                     Country country = repository.findByCode(city.getCountry());
+                    System.out.println("Cities..4"); 
                     country.getCities().add(city);
                     repository.save(country);                
                     System.out.println(city);   
                 });
             }
+        } else {
+            System.out.println("Cities..ONT EMPTY!!!"); 
         }
 
         repository.findAll().forEach((country) -> {
