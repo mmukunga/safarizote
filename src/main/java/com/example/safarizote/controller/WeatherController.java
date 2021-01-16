d9a56130b86c930488b1186b397f568b0665cd11
Simon Mukunga <mmukunga@gmail.com>
2021-01-16 17:37:47 +0100

    first commit

--- src/main/java/com/example/safarizote/controller/WeatherController.java
+++ src/main/java/com/example/safarizote/controller/WeatherController.java
@@ -1,7 +1,9 @@
 package com.example.safarizote.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.example.safarizote.model.City;
 import com.example.safarizote.model.Country;
 import com.example.safarizote.repository.CountryRepository;
 
@@ -20,8 +22,13 @@ public class WeatherController {
     @RequestMapping(value="/api/getCountries",  method={RequestMethod.GET})       
     public ResponseEntity<List<Country>> getCountries() { 
         System.out.println("Countries..");    
-        //List<Country> countries = repository.findAll();   
+        //List<Country> countries = repository.findAll();  
+        List<Country> countries = new ArrayList<>();   
+        Country temp = Country.builder()
+        .name("kENYA").code("KE")
+        .cities(new ArrayList<City>()).build();
+        countries.add(temp);
         //System.out.println("Countries:=" + countries.size());    
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        return new ResponseEntity<>(countries, HttpStatus.OK);
     }
 }    
--- webapp/src/pages/Weather.jsx
+++ webapp/src/pages/Weather.jsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react'
 
 const Weather = () => {
     const [names, setNames] = useState(["joe", "steve", "carol"])
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/countries')
+            .then(res => setData(res.data))
+            .catch(err => console.log(err))
+    }, []);
+
+    console.log(data);
 
     return (
         <div>
