8205f64157af9bd0dcffd3da10c1785a443e2b0b
Simon Mukunga <mmukunga@gmail.com>
2021-01-16 17:29:25 +0100

    first commit

--- webapp/src/pages/Weather.jsx
+++ webapp/src/pages/Weather.jsx
@@ -1,76 +1,14 @@
-import React, { useReducer, useEffect, useState } from 'react';
-import UserForm, { Select } from "./UserForm";
-import axios from 'axios';
-
-const initialState = {
-    country: {},
-    city: {}
-  };
-  
-  const reducer = (state, action) => {
-    if (action.type === "reset") {
-        return initialState;
-    }
-  
-    const result = { ...state };
-    result[action.type] = action.value;
-    return result;
-  };
+import React, { useState } from 'react'
 
 const Weather = () => {
-    const [countries, setCountries] = useState([]);
-    const [cities, setCities] = useState([]);
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [weather, setWeather] = useState({});
-    const [forecast, setForecast] = useState([]);
+    const [names, setNames] = useState(["joe", "steve", "carol"])
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log('Weather Forecast!!');
-        dispatch({ type: "reset" });
-    };
-    
-    const onChange = e => {
-      const { name, value } = e.target;
-      dispatch({ type: name, value });
-    };
-
-    useEffect(() => {
-        axios.get('/api/countries').then(response => {
-            console.log(response);
-            setCountries(response.data);
-            console.log('Countries OK!!');
-        }).catch(err => {
-            console.log(err);
-        });
-    }, []);
-
-    useEffect(() => {
-        axios.post('/api/cities', state.country).then(response => {
-            console.log(response);
-            setCities(response.data);
-            console.log('Cities OK!!');
-        }).catch(err => {
-            console.log(err);
-        });
-    }, [state.country]);
-
-    return (       
-        <div className="Weather">
-            Weather!
-            <UserForm
-                cancel={() => {console.log('cancel')}}
-                errors={[]}
-                onChange={onChange}
-                submit={handleSubmit}
-                elements={() => (
-                    <>
-                        <Select id="country" name="country" data={countries} text="Country" onChange={onChange}/>
-                        <Select id="city" name="country" data={cities} text="City" onChange={onChange}/>                    
-                    </>
-                )}
-                >    
-                </UserForm>
+    return (
+        <div>
+            <div>
+                { names.map((name, i) => <h1 key={i}>{name}</h1>) }
+            </div>
+            <button onClick={() => setNames(prev => [...prev, "frank"])}>Add Frank</button>
         </div>
     )
 }
