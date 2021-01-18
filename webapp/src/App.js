1187e8ea7151ea7f89fb62207422ca8a5192d65f
Simon Mukunga <mmukunga@gmail.com>
2021-01-19 00:12:04 +0100

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -27,7 +27,7 @@ function App() {
  
   return (
     <div className="App">
-       <Card fontColor="black" backgroundColor="grey" title="Safari Zote">
+       <Card fontColor="black" backgroundColor="#f0ffff" title="Safari Zote">
        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="App-title">{message.url}</p>
