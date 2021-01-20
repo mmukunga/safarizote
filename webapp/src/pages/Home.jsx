a34dafe7ccd3008a63b1f062b78be78751931425
Simon Mukunga <mmukunga@gmail.com>
2021-01-20 22:29:06 +0100

    first commit

--- webapp/src/pages/Home.jsx
+++ webapp/src/pages/Home.jsx
@@ -1,11 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from './Card';
+import Progress from './Progress';
 
 const Home = () => {
     const [data, setData] = useState([]);
     const [clientInfo, setClientInfo] = useState({})
     
+    const [progress, setProgress] = useState(0);
+    const [color, setColor] = useState('');
+    const colorArray = ['#7ea9e1', "#ed004f", "#00fcf0", "#d2fc00", "#7bff00", "#fa6900"];
+
+    const randomColor = () => {
+      return colorArray[Math.floor(Math.random() * colorArray.length)];
+    }
+
+    const randomProgressValue = () => {
+      const progressValue = Math.floor(Math.random() * 101);
+      setProgress(progressValue);
+      const randomProgressColor = randomColor();
+      setColor(randomProgressColor);
+    }
+    
+    useEffect(() => {
+        // Good!
+        document.title = 'Greetings page'; // Side-effect!
+        var rProgress = Math.floor(Math.random() * 100) + 1 ;
+        setProgress(rProgress);
+        const randomProgressColor = randomColor();
+        setColor(randomProgressColor);
+      }, []);
+
+
+
    useEffect(() => {
       axios.get('https://extreme-ip-lookup.com/json/')
         .then(response => {
@@ -28,6 +55,13 @@ const Home = () => {
     return (
         <Card title="Home" text="The Home - Sipili!!">
             <div className="vlContainer">
+                <Progress 
+                  progress={progress}
+                  size={500}
+                  strokeWidth={15}
+                  circleOneStroke='#7ea9e1'
+                  circleTwoStroke={color}
+                />
                 <ul className="vList">          
                     { data.map(item => <li key={item.id}>Link: {item.url} Browser: {item.browser}</li>) }
                 </ul>
