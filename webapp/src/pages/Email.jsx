99a8d47e31e0ddad6cddd05691f4970aa4352a3b
Simon Mukunga <mmukunga@gmail.com>
2021-01-29 00:06:20 +0100

    first commit

--- webapp/src/pages/Email.jsx
+++ webapp/src/pages/Email.jsx
@@ -1,64 +1,42 @@
-import React, { useState, useEffect } from "react";
-import UserForm, { InputField, TextArea } from "./UserForm";
+import React from 'react';
 import axios from 'axios';
 
-const initialState = {
-  name: "Simon Mukunga",
-  email: "mkunsim@gmail.com",
-  message: "Email- Just Testing!! Great Stuff!!",
-  dateCreated: new Date()
-};
-
 const Email = () => {
-  const [formData, setFormData] = useState(initialState);
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    axios.get("/api/email")
-        .then(response => {
-          setUsers(response.data)
-        })
-  }, []);
-
-  const handleChange = (event) => {
-    setFormData((prevProps) => ({
-      ...prevProps,
-      [event.target.name]: event.target.value
-    }));
-  };
-  
-  const clearState = () => {
-    setFormData({ ...initialState });
-  };
-
-  const handleSubmit = async (e) => {
-    axios.post("/api/sendEmail", formData)
-      .then(response => {
-          clearState();
-      }).catch(error => {
-          console.log(error);
-      });  
-  };
-
-  return (
-    <div className="Email">
-    <p>Please send us a Message..</p>
-    <UserForm
-      cancel={() => {console.log('cancel')}}
-      errors={[]}
-      onChange={handleChange}
-      submit={handleSubmit}
-      elements={() => (
-        <>        
-          <InputField name="name" text="Name:" type="text" onChange={handleChange} placeholder="Write here.."/>
-          <InputField name="email" text="E-mail:" type="text" onChange={handleChange} placeholder="Write here.."/>
-          <TextArea name="message" text="Message:" rows="4" cols="50" onChange={handleChange} placeholder="Write here.."/>
-        </>
-      )}
-    >    
-    </UserForm>
-    </div>
-  );
-};
+    const [email, setEmail] = React.useState('');
+    const [message, setMessage] = React.useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('/api/email', {
+            email: email,
+            message: message
+        }).then(response => {
+            console.log(response)
+        });
+    }
+    return (
+        <div>
+            <h1>Contact Us!</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type="message"
+                    name="message"
+                    placeholder="Message"
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                    required
+                />
+                <button type="submit">Send</button>  
+            </form>
+        </div>
+    );
+}
 
 export default Email;
