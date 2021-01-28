c66b7f658a7844cb860edd76bd1b9b4407a1596b
Simon Mukunga <mmukunga@gmail.com>
2021-01-29 00:22:39 +0100

    first commit

--- webapp/src/pages/Weather.jsx
+++ webapp/src/pages/Weather.jsx
@@ -1,247 +1,43 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import UserForm, { Select } from "./UserForm";
+import React from 'react';
 import axios from 'axios';
 
-  const initialState = {
-      id: '',
-      countryCode: 'AF',
-      countryName: 'Afghanistan',
-      cityName: 'Kabul'
-  };
-  
-  const reducer = (state, action) => {
-    switch(action.type) {
-      case 'SET_COUNTRY':
-        return {
-          ...state, ...action.payload
-        };
-      case 'SET_CITY':
-        return { 
-          ...state, cityName: action.payload
-        };  
-      default:
-        return state;
-    }
-  }
-
-  const IMG_URL = 'https://openweathermap.org/img/w';
-  
-  const Weather = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [countries, setCountries] = useState([]);
-    const [cities, setCities] = useState([]);
-    const [weather, setWeather] = useState({});
-    const [forecast, setForecast] = useState([]);
-    const [list, setList] = useState([]);
-    
-
-    function handleChange(evt) {
-      if (evt.target.name === "countryCode") {
-        const filteredCountry = countries.find(country => country.value === evt.target.value);
-        let newCountry = { ...state };
-        newCountry = { ...state, 
-          countryCode: filteredCountry.value, 
-          countryName: filteredCountry.title
-        };
-        dispatch({ type: 'SET_COUNTRY', payload: newCountry});
-      } else {
-        dispatch({ type: 'SET_CITY', payload: evt.target.value });
-      }
-    }
-    
-    useEffect(() => {
-      axios.get('/api/countries')
-          .then(response => {
-              let filteredCountries = [];
-              response.data.forEach(function(d) {
-                filteredCountries.push({
-                   id: d.id,   
-                   title: d.name,
-                   value: d.code
-                 });
-               }); 
-              setCountries(filteredCountries);
-          }).catch(err => console.log(err))
-  }, []);
-
-  React.useEffect(() => {
-
-      let country = {
-            name: state.countryName,
-            code: state.countryCode,
-            cities: null
-      };
-
-      axios.post('/api/cities', country)
-          .then(response => {
-              let filteredCity = [];
-              response.data.forEach(function(city) {
-                filteredCity.push({  
-                   id: city.id,  
-                   value: city.name,
-                   title: city.name
-                 });
-               }); 
-              setCities(filteredCity);
-          }).catch(err => console.log(err))
-  }, [state.countryCode]);
-
-    React.useEffect(() => {
-      const country = {
-          name: state.countryName,
-          code: state.countryCode,
-          cities: null
-      };
-        
-      var headers = {
-        'Content-Type': 'application/json' 
-      };
-
-      const fetchData = async () => {
-        axios.post('/api/weather', country, headers).then(response => {
-          setWeather(response.data);
-      }).catch(err => {
-          console.log(err);
-      });
-
-      axios.post('/api/forecast', country, headers).then(response => {
-          setList(response.data.list);
-          setForecast(response.data);
-      }).catch(err => {
-          console.log(err);
-      });
-      };
-
-      fetchData();
-
-    }, []);
-
-    const handleSubmit = e => {
-        e.preventDefault();    
-        var headers = {
-          'Content-Type': 'application/json' 
-        };
-
-        const country = {
-            name: state.cityName,
-            code: state.countryCode,
-            cities: null
-        };
-
-        axios.post('/api/weather', country, headers).then(response => {
-            setWeather(response.data);
-        }).catch(err => {
-            console.log(err);
+const Weather = (props) => {
+    const [country, setCountry] = React.useState('');
+    const [city, setCity] = React.useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('/api/weather', {
+            country: country,
+            city: city
+        }).then(response => {
+            console.log(response)
         });
-
-        axios.post('/api/forecast', country, headers).then(response => {
-            setList(response.data.list);
-            setForecast(response.data);
-        }).catch(err => {
-            console.log(err);
-        });
-
-    };
-    
-
-    const WeatherCard = (props) => {
-        return ( 
-          <div className="WeatherCard"> 
-              <span>loc: {props.weather.loc} date: {props.weather.date}</span>
-              <img src={`${IMG_URL}/${props.weather.weather[0].icon}.png`} alt="wthr img" className="wthrImg"/>
-              <div className="Cells">
-                <div className="Cell">
-                  <small>Current</small>
-                  <div>{props.weather.main.temp}°</div>
-                </div>
-                <div className="Cell">
-                  <small>Sky</small>
-                  <div>{props.weather.weather[0].main}</div>
-                </div>
-                <div className="Cell">
-                  <div>{props.weather.weather[0].description}</div>
-                </div>
-              </div>
-          </div>
-        )
-      }
-
-      const ForecastCard = (props) => {
-        return (
-            <div className="ForecastCard">
-              <span>{props.date}</span>
-              <img src={`${IMG_URL}/${props.weather[0].icon}.png`} alt="wthr img" className="wthrImg"/>
-              <div className="Cells">
-                <div className="Cell">
-                  <small>High</small>
-                  <div>{props.temp.day}°</div>
-                </div>
-                <div className="Cell">
-                  <small>Now</small>
-                  <div>{props.temp.min}°</div>
-                </div>
-                <div className="Cell">
-                  <small>Main</small>
-                  <div>{props.weather[0].main}</div>
-                </div>
-                <div className="Cell">
-                  <small>&nbsp;</small>
-                  <div>{props.weather[0].description}</div>
-                </div>
-              </div>
-            </div>
-        )
-      }
-     
-    const ForecastList = props => {
-      return (
-      <div className="ForecastList">
-          {props.cards.map(card => (
-              card.temp !=null && <ForecastCard {...card} />
-          ))}
-      </div>
-      )
-    };
-
+    }
     return (
-        <div className="Weather">
-            Weather! {state.cityName} - {state.countryName}, {state.countryCode} 
-            <UserForm
-                cancel={() => {console.log('cancel')}}
-                errors={[]}
-                onChange={handleChange}
-                submit={handleSubmit}
-                elements={() => (
-                   <>
-                      <Select id="countryCode" name="countryCode" text="Country"  data={countries} onChange={handleChange}/>
-                      <Select id="cityName" name="cityName" text="City" data={cities} onChange={handleChange}/>                    
-                   </>
-                )}
-                >    
-            </UserForm>
-
-            <div className="day-container">
-                {weather != null && weather.main
-                 ?  <WeatherCard 
-                    title='Current Local Weather'
-                    width='500px'
-                    date={new Date((weather.dt)*1000).toLocaleDateString("en-US")}
-                    weather={weather}
-                    />     
-                  : <div>No Weather</div>   
-              }
-            </div>
-
-            <div className="5days-container">
-                {forecast != null 
-                  ? <ForecastList cards={list} />
-                  : <div>No Forecast</div>   
-              }
-            </div>
-
-
+        <div>
+            <h1>Weather</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="country"
+                    name="country"
+                    placeholder="Country"
+                    value={country}
+                    onChange={e => setCountry(e.target.value)}
+                    required
+                />
+                <input
+                    type="city"
+                    name="city"
+                    placeholder="City"
+                    value={city}
+                    onChange={e => setCity(e.target.value)}
+                    required
+                />
+                <button type="submit">Send</button>  
+            </form>
         </div>
     );
 }
 
-export default Weather
+export default Weather;
+
