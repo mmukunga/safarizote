a3c8f79c57fe4d30c8ed757d680c3cf287546fdf
Simon Mukunga <mmukunga@gmail.com>
2021-01-29 11:43:10 +0100

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -4,7 +4,8 @@ const Card = (props) => {
     const style = {
         margin: '0 auto',
         padding: '5px',
-       /* border: '2px solid lightgrey' */
+        border: '2px solid #f8ecd5',
+        boxShadow: '0 0 10px 5p' 
     }
  
     return (
