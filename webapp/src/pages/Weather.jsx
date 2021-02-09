cd267cc048f80528d5a11d3e2494ac51e6a463ef
Simon Mukunga <mmukunga@gmail.com>
2021-02-10 00:41:19 +0100

    first commit

--- webapp/src/pages/Weather.jsx
+++ webapp/src/pages/Weather.jsx
@@ -146,6 +146,17 @@ import Card from './Card';
     return (
         <Card cardWidth="500px" fontColor="black" backgroundColor="#F0FFFF">
         {state.cityName} - {state.countryName}, {state.countryCode} 
+
+        <form class="form-inline" action="/action_page.php">
+          <label for="email">Email:</label>
+          <input type="email" id="email" placeholder="Enter email" name="email"/>
+          <label for="pwd">Password:</label>
+          <input type="password" id="pwd" placeholder="Enter password" name="pswd"/>
+          <button type="submit">Submit</button>
+        </form>
+
+
+
         <form className="form-container" onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-25">   
