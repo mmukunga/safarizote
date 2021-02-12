dab29de718e8815e274d6ecbf89847c6d2c180d1
Simon Mukunga <mmukunga@gmail.com>
2021-02-12 14:08:11 +0100

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
  
 const Card = (props) => {
 
-  const { cardWidth, fontColor, backgroundColor, children } = props;
+  const { cardWidth, fontColor, backgroundColor, borderColor,  children } = props;
   const style = {
     width: cardWidth,
     backgroundColor: backgroundColor,
@@ -10,11 +10,12 @@ const Card = (props) => {
     borderRadius: 10,
     margin: '10px auto',
     padding: '1px',
-    border: '4px solid #A4FFFF',
+    border: '4px solid #F0FFFF',
     boxShadow: '0 0 10px 5p' 
   };
 
-  const labelStyles = { color: fontColor };
+  const border = '4px solid ' + { borderColor };
+  const labelStyles = { color: fontColor, border: '4px solid red'};
  
     return (
       <div style={style}>   
