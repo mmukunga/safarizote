6eb1ffa9262d18f9818090aa33ab4e5f56eaf4d2
Simon Mukunga <mmukunga@gmail.com>
2021-02-17 19:32:03 +0100

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -4,43 +4,46 @@ import Card from './Card';
 
   const tree = [{"name":"Root Node","collapsed":true,"nodes":[{"name":"Node 1","collapsed":true,"nodes":[{"name":"Sub node"}]},{"name":"Node 2","collapsed":true,"nodes":[{"name":"Sub node "}]},{"name":"Node 3","collapsed":true,"nodes":[{"name":"Sub node"}]}]}];
   const BackUp = () => {
-
+    const [checkedItems, setCheckedItems] = useState({});
+    
+    const handleChange = (event) => {
+      setCheckedItems({...checkedItems, [event.target.name] : event.target.checked });
+    }
+    
+    useEffect(() => {
+      console.log("checkedItems: ", checkedItems);
+    }, [checkedItems]);  
+      
     console.log('ClickMeg6');
+    
+    function TreeList(props) {
+      const {list} = props;
+      return <div>{list.map(f => <TreeItem key={f.name} item={f} handleChange={props.handleChange}/>)}</div>;
+    }
 
     function TreeItem(props) {
       const {item} = props;
       const [collapsed, setCollapsed] = useState(item.collapsed);
-      const [state, setState] = useState({isGoing: true});  
-      const [checkedItems, setCheckedItems] = useState({}); //plain object as state
 
       const handleChange = (event) => {
-        setCheckedItems({...checkedItems, [event.target.name] : event.target.checked });
+        props.handleChange(event);
       }
 
-      useEffect(() => {
-        console.log("checkedItems: ", checkedItems);
-      }, [checkedItems]);  
-        
       return <div className="item">
         <input name={item.name} type="checkbox" checked={checkedItems[item.name]} onChange={handleChange}/> &nbsp; &nbsp;
         <span onClick={() => setCollapsed(!collapsed)}>{item.name}</span> 
         {!collapsed && item.nodes && 
           <div style={{paddingLeft: "1rem"}}>
-            <TreeList list={item.nodes}/>
+            <TreeList list={item.nodes} handleChange={props.handleChange}/>
           </div>
         }
       </div>
-    }
-    
-    function TreeList(props) {
-      const {list} = props;
-      return <div>{list.map(f => <TreeItem key={f.name} item={f}/>)}</div>;
     }
 
     return (
       <Card cardWidth="500px" fontColor="black" backgroundColor="#F0FFFF">
           <h1>Tree BackUp</h1>
-          <TreeList list={tree}/>
+          <TreeList list={tree} handleChange={handleChange}/>
       </Card>
     );
   }
