0dbe7ab20dd5604efbbd133be01d20120615056b
Simon Mukunga <mmukunga@gmail.com>
2021-02-17 19:01:53 +0100

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -4,24 +4,25 @@ import Card from './Card';
 
   const tree = [{"name":"Root Node","collapsed":true,"nodes":[{"name":"Node 1","collapsed":true,"nodes":[{"name":"Sub node"}]},{"name":"Node 2","collapsed":true,"nodes":[{"name":"Sub node "}]},{"name":"Node 3","collapsed":true,"nodes":[{"name":"Sub node"}]}]}];
   const BackUp = () => {
-    
+
     console.log('ClickMeg6');
 
     function TreeItem(props) {
       const {item} = props;
       const [collapsed, setCollapsed] = useState(item.collapsed);
-      const [state, setState] = useState({isGoing: true});
+      const [state, setState] = useState({isGoing: true});  
+      const [checkedItems, setCheckedItems] = useState({}); //plain object as state
 
       const handleChange = (event) => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name  = target.name;
-        console.log(target);
-        setState({[name]: value});
+        setCheckedItems({...checkedItems, [event.target.name] : event.target.checked });
       }
 
+      useEffect(() => {
+        console.log("checkedItems: ", checkedItems);
+      }, [checkedItems]);  
+        
       return <div className="item">
-        <input name={item.name} type="checkbox" checked={state.isGoing} onChange={handleChange}/> &nbsp; &nbsp;
+        <input name={item.name} type="checkbox" checked={checkedItems[item.name]} onChange={handleChange}/> &nbsp; &nbsp;
         <span onClick={() => setCollapsed(!collapsed)}>{item.name}</span> 
         {!collapsed && item.nodes && 
           <div style={{paddingLeft: "1rem"}}>
