b9ad74c8d6a39ff62a2d3a78bcf7819e858878a3
Simon Mukunga <mmukunga@gmail.com>
2021-02-18 23:52:26 +0100

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from "react";
 import Card from './Card';
 
   const tree = [
-    {"name":"Desktop(this PC)","collapsed":true,
+    {"name":"Desktop(this PC)", "id": "00", "collapsed":true,
      "nodes":[
-       {"name":"C:\\SimTemps","collapsed":true,
+       {"name":"C:\\SimTemps", "id": "01","collapsed":true,
         "nodes":[
-          {"name":"D:\\SimTemps"},
-          {"name":"E:\\SimTemps"}
+          {"name":"D:\\SimTemps", "id": "11"},
+          {"name":"E:\\SimTemps", "id": "12"}
          ]
        },
-       {"name":"C:\\Projects","collapsed":true,
+       {"name":"C:\\Projects", "id": "02","collapsed":true,
         "nodes":[
-          {"name":"D:\\Projects"},
-          {"name":"E:\\Projects"}
+          {"name":"D:\\Projects", "id": "21"},
+          {"name":"E:\\Projects", "id": "22"}
          ]
        },
-       {"name":"C:\\FamilieAlbum","collapsed":true,
+       {"name":"C:\\FamilieAlbum", "id": "03","collapsed":true,
         "nodes":[
-          {"name":"D:\\FamilieAlbum"},
-          {"name":"E:\\FamilieAlbum"}
+          {"name":"D:\\FamilieAlbum", "id": "31"},
+          {"name":"E:\\FamilieAlbum", "id": "32"}
          ]
         }
       ]}
@@ -29,7 +29,17 @@ import Card from './Card';
     const [collapsed, setCollapsed] = useState(true);
 
     const handleChange = (event) => {
-      setCheckedItems({...checkedItems, [event.target.name] : event.target.checked });
+      const {name, value} = event.target;
+
+      var array = [...checkedItems];
+      //var index = array.indexOf(e.target.name);
+      const itemFound= array.findIndex(x => x.name === name);
+
+      if (itemFound !== -1) {
+        setCheckedItems(checkedItems.filter(item => item.name !== name));
+      } else {
+        setCheckedItems({...checkedItems, [name] : event.target.checked });
+      }
     }
     
     const handleCollapsed = (collapsed) => {
@@ -55,7 +65,7 @@ import Card from './Card';
       }, [checkedItems]);  
         
       return <div className="item">
-        <input name={item.name} type="checkbox" checked={checkedItems[item.name]} onChange={handleChange}/> &nbsp; &nbsp;
+        <input name={item.name} id={item.id} type="checkbox" checked={checkedItems[item.name]} onChange={handleChange}/> &nbsp; &nbsp;
         <span onClick={handleCollapsed}>{item.name}</span> 
         {!props.collapsed && item.nodes && 
           <div style={{paddingLeft: "1rem"}}>
