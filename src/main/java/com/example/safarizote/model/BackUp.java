825e56a7957448c64a9f6564f30e3df009434959
Simon Mukunga <mmukunga@gmail.com>
2021-02-25 21:58:09 +0100

    first commit

--- src/main/java/com/example/safarizote/model/BackUp.java
+++ src/main/java/com/example/safarizote/model/BackUp.java
@@ -38,5 +38,5 @@ public class BackUp {
     BackUp parent;
     @OneToMany(mappedBy="parent", fetch=FetchType.EAGER)
     @EqualsAndHashCode.Exclude
-    Set<BackUp> children = new HashSet<>();
+    Set<BackUp> children;
 }
