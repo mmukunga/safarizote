f3c55ae14d2a213d056a1dee5ab7a2776cf11730
Simon Mukunga <mmukunga@gmail.com>
2021-02-27 00:40:20 +0100

    first commit

--- src/main/java/com/example/safarizote/repository/BackUpLoader.java
+++ src/main/java/com/example/safarizote/repository/BackUpLoader.java
@@ -18,16 +18,18 @@ import com.example.safarizote.model.BackUp;
 @Component
 public class BackUpLoader implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(BackUpLoader.class);
-
     @Autowired
     private BackUpRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("BackUpLoader..root...");
+        if (repository.count() > 0) {
+           System.out.println("..TABLE ONT EMPTY!!..");
+           return;
+        }
         BackUp rootFolder = BackUp.builder().name("root").parent(null).dateCreated(Instant.now()).build();
         System.out.println("BackUpLoader..root..." + rootFolder);
-
         List<String> folders = Arrays.asList("SimTemps", "Projects", "FamilieAlbum");
         Set<BackUp> children = folders.stream().map(title -> BackUp.builder()
                 .name(title)
@@ -37,7 +39,6 @@ public class BackUpLoader implements CommandLineRunner {
                 .build())
                 .collect(Collectors.toSet());    
         System.out.println("BackUpLoader..root...");
-
         BackUp root = repository.save(BackUp.builder()
         .name(rootFolder.getName())
         .collapsed(true)
@@ -45,8 +46,26 @@ public class BackUpLoader implements CommandLineRunner {
         .children(children)
         .build());        
 
-        System.out.println("BackUpLoader...root..." + root);
-        System.out.println("BackUpLoader...");
+        System.out.println("BackUpLoader...SimTemps..." + root);
+        BackUp simTempsFolder = repository.findByName("SimTemps");
+        List<String> simTemps = Arrays.asList("DSimTemps", "ESimTemps");
+        Set<BackUp> simTempsChildren = simTemps.stream().map(title -> BackUp.builder()
+                .name(title)
+                .parent(simTempsFolder) 
+                .collapsed(true)
+                .dateCreated(Instant.now())
+                .build())
+                .collect(Collectors.toSet()); 
+        System.out.println("BackUpLoader..SimTemps...");
+        BackUp rootSimTemps = repository.save(BackUp.builder()
+                .name(simTempsFolder.getName())
+                .collapsed(true)
+                .dateCreated(Instant.now())
+                .children(simTempsChildren)
+                .build());        
+        System.out.println("BackUpLoader..SimTemps..." + rootSimTemps);
+
+        System.out.println("BackUpLoader...OK!");
         repository.findAll().forEach((backUp) -> {
             logger.info("{}", backUp);
         });
