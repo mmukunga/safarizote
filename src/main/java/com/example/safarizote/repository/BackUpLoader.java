5610e98cd819604f2deb1c1e5387f224916b297a
Simon Mukunga <mmukunga@gmail.com>
2021-02-28 21:41:10 +0100

    first commit

--- src/main/java/com/example/safarizote/repository/BackUpLoader.java
+++ src/main/java/com/example/safarizote/repository/BackUpLoader.java
@@ -30,7 +30,7 @@ public class BackUpLoader implements CommandLineRunner {
            return;
         }
         BackUp rootFolder = BackUp.builder().name("root").parent(null).dateCreated(Instant.now()).build();
-        System.out.println("BackUpLoader..root...2" + rootFolder);
+        System.out.println("BackUpLoader..rootFolder..." + rootFolder);
         List<String> folders = Arrays.asList("SimTemps", "Projects", "FamilieAlbum");
         Set<BackUp> children = folders.stream().map(title -> BackUp.builder()
                 .name(title)
@@ -40,7 +40,7 @@ public class BackUpLoader implements CommandLineRunner {
                 .build())
                 .collect(Collectors.toSet()); 
                    
-        System.out.println("BackUpLoader..root...3");
+        System.out.println("BackUpLoader..children..." + children);
         BackUp root = repository.save(BackUp.builder()
                 .name(rootFolder.getName())
                 .collapsed(true)
@@ -50,62 +50,30 @@ public class BackUpLoader implements CommandLineRunner {
         System.out.println("BackUpLoader...root...4" + root);
 
         BackUp simTempsFolder = repository.findByName("SimTemps");
-        System.out.println("BackUpLoader...SimTemps...5" + simTempsFolder);
+        System.out.println("BackUpLoader...simTempsFolder..." + simTempsFolder);
 
         List<String> simTemps = Arrays.asList("DSimTemps", "ESimTemps");
         Set<BackUp> simTempsChildren = simTemps.stream().map(title -> BackUp.builder()
-                .name(title)
-                .parent(simTempsFolder) 
-                .collapsed(true)
-                .dateCreated(Instant.now())
-                .build())
-                .collect(Collectors.toSet()); 
-        System.out.println("BackUpLoader..SimTemps...6");
-        BackUp rootSimTemps = repository.save(BackUp.builder()
-                .name(simTempsFolder.getName())
-                .collapsed(true)
-                .dateCreated(Instant.now())
-                .children(simTempsChildren)
-                .build());        
-        System.out.println("BackUpLoader..SimTemps...7" + rootSimTemps);
+            .name(title)
+            .parent(simTempsFolder) 
+            .collapsed(true)
+            .dateCreated(Instant.now())
+            .build())
+            .collect(Collectors.toSet()); 
+        System.out.println("BackUpLoader..simTempsChildren... " + simTempsChildren);
 
-        BackUp projectsFolder = repository.findByName("Projects");
-        System.out.println("BackUpLoader...Projects...5" + projectsFolder);
-        List<String> projects = Arrays.asList("DProjects", "EProjects");
-        Set<BackUp> projectsChildren = projects.stream().map(title -> BackUp.builder()
-                .name(title)
-                .parent(projectsFolder) 
-                .collapsed(true)
-                .dateCreated(Instant.now())
-                .build())
-                .collect(Collectors.toSet()); 
-        System.out.println("BackUpLoader..Projects...6");
-        BackUp rootProjects = repository.save(BackUp.builder()
-                .name(projectsFolder.getName())
-                .collapsed(true)
-                .dateCreated(Instant.now())
-                .children(projectsChildren)
-                .build());        
-        System.out.println("BackUpLoader..Projects...7" + rootProjects);
+        simTempsFolder.setChildren(simTempsChildren);
+        BackUp rootSimTemps = repository.save(simTempsFolder); 
+        System.out.println("BackUpLoader..rootSimTemps... " + rootSimTemps);
 
-        BackUp familieAlbumFolder = repository.findByName("FamilieAlbum");
-        System.out.println("BackUpLoader...FamilieAlbum...5" + familieAlbumFolder);
-        List<String> familieAlbum = Arrays.asList("DFamilieAlbum", "EFamilieAlbum");
-        Set<BackUp> familieAlbumChildren = familieAlbum.stream().map(title -> BackUp.builder()
-                .name(title)
-                .parent(familieAlbumFolder) 
-                .collapsed(true)
-                .dateCreated(Instant.now())
-                .build())
-                .collect(Collectors.toSet()); 
-        System.out.println("BackUpLoader..FamilieAlbum...6");
+        /*
         BackUp rootFamilieAlbum = repository.save(BackUp.builder()
-                .name(familieAlbumFolder.getName())
-                .collapsed(true)
-                .dateCreated(Instant.now())
-                .children(familieAlbumChildren)
-                .build());        
-        System.out.println("BackUpLoader..FamilieAlbum...7" + rootFamilieAlbum);
+            .name(familieAlbumFolder.getName())
+            .collapsed(true)
+            .dateCreated(Instant.now())
+            .children(familieAlbumChildren)
+            .build());   
+        */
 
         System.out.println("BackUpLoader...OK!");
         repository.findAll().forEach((backUp) -> {
