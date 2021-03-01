54631a9251d074242002f17bf8228dc5b2addcdd
Simon Mukunga <mmukunga@gmail.com>
2021-03-01 20:48:32 +0100

    first commit

--- src/main/java/com/example/safarizote/repository/BackUpRepository.java
+++ src/main/java/com/example/safarizote/repository/BackUpRepository.java
@@ -1,13 +1,12 @@
 package com.example.safarizote.repository;
 
-import java.util.Set;
-
 import com.example.safarizote.model.BackUp;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface BackUpRepository extends JpaRepository<BackUp,Integer> {
     BackUp findByName(String name);
 }
