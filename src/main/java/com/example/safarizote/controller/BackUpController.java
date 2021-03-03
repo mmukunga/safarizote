74337b97ee36cefd8838d74fe1eae7744be3f7e7
Simon Mukunga <mmukunga@gmail.com>
2021-03-03 18:42:00 +0100

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -12,28 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.transaction.annotation.Transactional;
-import com.example.safarizote.model.BackUp;
-import com.example.safarizote.repository.BackUpRepository;
+import com.example.safarizote.model.Category;
+import com.example.safarizote.repository.CategoryRepository;
 
 @RestController
 public class BackUpController { 
   @Autowired
-  private BackUpRepository repository;
+  private CategoryRepository repository;
     
     @Transactional
     @RequestMapping(value = "/api/backUp",  method={RequestMethod.GET})
-    public ResponseEntity<List<BackUp>> findAll() {
-        System.out.println("BackUp.findAll() , the time at the server is now " + new Date());
-        List<BackUp> backUps = repository.findAll();
-        System.out.println("BackUp.findAll() , the time at the server is now " + new Date());
+    public ResponseEntity<List<Category>> findAll() {
+        System.out.println("Category.findAll() , the time at the server is now " + new Date());
+        List<Category> backUps = repository.findAll();
+        System.out.println("Category.findAll() , the time at the server is now " + new Date());
 
         // New Enhanced For loop
         System.out.println("\n==============> 1. New Enhanced For loop Example..");
-        for (BackUp temp : backUps) {
+        for (Category temp : backUps) {
             System.out.println(temp);
         }
         System.out.println("\n==============> 2. New Enhanced For loop Example..");
-        System.out.println("BackUp.findAll()  End OK!");
+        System.out.println("Category.findAll()  End OK!");
         return new ResponseEntity<>(backUps, HttpStatus.OK);
     }
 }
