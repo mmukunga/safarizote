8e79688fbf0eeb8c4a26bb35acb5214912339e33
Simon Mukunga <mmukunga@gmail.com>
2021-03-06 21:18:17 +0100

    first commit

--- webapp/src/pages/SignIn.jsx
+++ webapp/src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Card from './Card';
 import { Redirect } from "react-router-dom";
@@ -9,10 +9,10 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'SET_EMAIL':
-            const {name, value} = action.payload;
-            return {...state, [name]: value};
+            const { name, value } = action.payload;
+            return { ...state, [name]: value };
         default:
             return state;
     }
@@ -25,27 +25,27 @@ const SignIn = (props) => {
     const { from } = props.location.state || { from: { pathname: "/" } };
 
     const handleChange = (event) => {
-        dispatch({type: 'SET_EMAIL', payload: event.target})
+        dispatch({ type: 'SET_EMAIL', payload: event.target })
     }
 
     const handleSubmit = e => {
-      e.preventDefault();
-      axios.post('/api/login', {
-        email: state.email,
-        password: state.password
-      }).then(response => {
-        console.log(response);
-        console.log(response.data);
-      }).catch(error => {
-        console.log(error);
-      }); 
+        e.preventDefault();
+        axios.post('/api/login', {
+            email: state.email,
+            password: state.password
+        }).then(response => {
+            console.log(response);
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        });
 
-      setCount(prevCount => prevCount + 1);
-      localStorage.setItem('token', state.email+count);
+        setCount(prevCount => prevCount + 1);
+        localStorage.setItem('token', state.email + count);
     };
-    
+
     console.log('COUNT:= ' + count);
-    console.log('FROM:= '  + from);
+    console.log('FROM:= ' + from);
 
     if (localStorage.getItem('token') != null) {
         const userToken = localStorage.getItem('token');
@@ -55,20 +55,20 @@ const SignIn = (props) => {
         console.log(from);
         console.log('..2.SignIn FROM..');
         return <Redirect to={from} />;
-    }  
+    }
 
     return (
         <Card cardWidth="500px" fontColor="black" backgroundColor="#F0FFFF">
-         Please Login Here!!
-        <form class="form-inline" onSubmit={handleSubmit}>
-          <input type="email" id="email" name="email" placeholder="Enter email"
-              onChange={handleChange} required/>
-          <input type="password" id="password" name="password" placeholder="Enter password"
-              onChange={handleChange} required/>
-          <input type="submit" name="Submit" />
-        </form>
-       </Card>
-     );
-  }
+            Please Login Here!!
+            <form class="form-inline" onSubmit={handleSubmit}>
+                <input type="email" id="email" name="email" placeholder="Enter email"
+                    onChange={handleChange} required />
+                <input type="password" id="password" name="password" placeholder="Enter password"
+                    onChange={handleChange} required />
+                <input type="submit" name="Submit" />
+            </form>
+        </Card>
+    );
+}
 
-  export default SignIn;
+export default SignIn;
