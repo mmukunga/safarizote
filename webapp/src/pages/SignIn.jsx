4b8d1b4f803e0e4d7b64db8835abe2ebf0a46c2e
Simon Mukunga <mmukunga@gmail.com>
2021-03-06 15:52:15 +0100

    first commit

--- webapp/src/pages/SignIn.jsx
+++ webapp/src/pages/SignIn.jsx
@@ -3,12 +3,31 @@ import axios from 'axios';
 import Card from './Card';
 import { Redirect } from "react-router-dom";
 
+const initialState = {
+    email: '',
+    password: ''
+};
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'SET_EMAIL':
+            const {name, value} = action.payload;
+            return {...state, [name]: value};
+        default:
+            return state;
+    }
+}
 
 const SignIn = (props) => {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
     const [count, setCount] = useState(0);
 
     const nameEl = React.useRef(null);
     const { from } = props.location.state || { from: { pathname: "/" } };
+
+    const handleChange = (event) => {
+        dispatch({type: 'SET_EMAIL', payload: event.target})
+    }
 
     const handleSubmit = e => {
       e.preventDefault();
@@ -23,6 +42,7 @@ const SignIn = (props) => {
     if (count > 5) {
         const userToken = localStorage.getItem('token');
         console.log(userToken);
+        console.log(state);
         console.log('SignIn FROM:= ' + from);
         return <Redirect to={from} />;
     }  
@@ -32,6 +52,10 @@ const SignIn = (props) => {
          <label>Name:
            <input type="text" ref={nameEl} />
          </label>
+         <input type="email" id="email" name="email" placeholder="Enter email"
+            onChange={handleChange} required/>
+         <input type="password" id="password" name="password" placeholder="Enter password"
+            onChange={handleChange} required/>
          <input type="submit" name="Submit" />
        </form>
      );
