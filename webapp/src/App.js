0a12f3d0e181939c6002a0d08cb83b1806c38ab7
Simon Mukunga <mmukunga@gmail.com>
2021-03-07 21:27:54 +0100

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Route, Switch, NavLink } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 
