e2eaa4b7a30ff497cdc9c4c3f04e2cdf1399ae90
Simon Mukunga <mmukunga@gmail.com>
2021-03-13 01:18:06 +0100

    first commit

--- webapp/src/pages/Shopping.jsx
+++ webapp/src/pages/Shopping.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from './Card';
-import gettyimages from '../images/tourist.jpg';
+import gettyimages from '../images/tourists.jpg';
 
 const initialState = {
     store: '',
