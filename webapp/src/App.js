0a89b8d69a20354653eb1fb0edfe7efa6010f106
Simon Mukunga <mmukunga@gmail.com>
2021-03-14 20:36:48 +0100

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -64,6 +64,8 @@ function App() {
   const Toolbar = (props) => {
 
     const images = [
+        './logo192.png',
+        './logo512.png',
         './images/leopard.jpg',
         './images/cheeter.jpg',
         './images/cheeters.jpg',
@@ -75,7 +77,7 @@ function App() {
     ];
 
     const id = Math.floor(Math.random() * (7 - 0) + 0);
-    const [selectedImage, setSelectedImage] = React.useState(images[id]);
+    const [selectedImage, setSelectedImage] = React.useState(images[0]);
     
     const imageStyles = {
       backgroundImage: `url(${selectedImage})`, 
