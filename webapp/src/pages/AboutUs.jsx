c3b8c9d6b9b6f586adc3c63e39e87a024908548a
Simon Mukunga <mmukunga@gmail.com>
2021-03-29 01:26:57 +0200

    first commit

--- webapp/src/pages/AboutUs.jsx
+++ webapp/src/pages/AboutUs.jsx
@@ -7,16 +7,18 @@ const AboutUs = () => {
         <Card className="InnerCard" fontColor="black" >
            <h4>About Us!</h4>
            <p>16 years in the Safari industry offered an opportunity to gain a wealth of experience, knowledge, 
-           enthusiasm and immense passion. Today Tufayn Mangal stands proud as a Professional Photographer,
-           Africa Safari Specialist and a Professional Safari Guide in flora and fauna.</p>
+              enthusiasm and immense passion. Today Tufayn Mangal stands proud as a Professional Photographer,
+              Africa Safari Specialist and a Professional Safari Guide in flora and fauna.</p>
            <p>Our Safari products have undergone a thorough research as we want to indulge a safari with real 
-           adventure. It’s our objective to involve our clients in daily activities where we share knowledge in 
-           wildlife and photography.</p>
+              adventure. It’s our objective to involve our clients in daily activities where we share knowledge in 
+              wildlife and photography.</p>
            <p>As you Explore our great Destinations we strive to showcase the diversity and the beauty of all 
-           it offers. We will go the extra mile to ensure every area visited is met with high expectations and
-           results.</p>
-           <strong>Karibu Sana</strong><br/>
-           <strong>Explore Safaris by Maji Ltd</strong>
+              it offers. We will go the extra mile to ensure every area visited is met with high expectations and
+              results.</p>
+           <p style={{textAlign:'left', padding:'10px'}}>  
+              <strong>Karibu Sana</strong><br/>
+              <strong>Explore Safaris by Maji Ltd</strong>
+           </p> 
            <ul className="vList">          
               {names.map((name, i) => <li key={i}>{name}</li>)}
            </ul>
