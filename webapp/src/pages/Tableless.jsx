2f42c2c9dde0236b71973864cb56a0c880bfe468
Simon Mukunga <mmukunga@gmail.com>
2021-04-01 20:05:50 +0200

    first commit

--- webapp/src/pages/Tableless.jsx
+++ webapp/src/pages/Tableless.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
 const Tableless = props => {
+    console.log('Mukunga1');
     const { cards } = props;
-    const Card = props => {
+    console.log('Mukunga2');
+    const Card = ({title, description}) => {
         console.log('Mukunga');
         return (
         <div style={{ margin: '1em' }}>            
             <div class="tableWrap">
-                <div class="tableCol">{props.title}</div>
-                <div class="tableCol">{props.description}</div>
+                <div class="tableCol">{title}</div>
+                <div class="tableCol">{description}</div>
             </div>
         </div>
         )
     }
     return(
     <div>
+        Macharia
       {cards && cards.data.map(card => (
         <Card {...card} />
       ))}
