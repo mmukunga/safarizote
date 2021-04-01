cfd21d8c668a3113a906eba2bdcdb0bdc1a88311
Simon Mukunga <mmukunga@gmail.com>
2021-04-01 22:12:02 +0200

    first commit

--- webapp/src/pages/Tableless.jsx
+++ webapp/src/pages/Tableless.jsx
@@ -12,9 +12,9 @@ const Tableless = props => {
         console.log('Mukunga4');
         return (
         <div style={{ margin: '1em' }}>            
-            <div class="tableWrap">
-                <div class="tableCol">{Number.isInteger(props.title) ? props.title : parse(props.title)}</div>
-                <div class="tableCol">{Number.isInteger(props.description) ? props.description : parse(props.description)}</div>
+            <div className="tableWrap">
+                <div className="tableCol">{Number.isInteger(props.title) ? props.title : parse(props.title)}</div>
+                <div className="tableCol">{Number.isInteger(props.description) ? props.description : parse(props.description)}</div>
             </div>
         </div>
         )
@@ -23,9 +23,9 @@ const Tableless = props => {
     <div>
       Macharia XX
       <div style={{ margin: '1em' }}>            
-            <div class="tableWrap"  style={{ background: 'green' }}>
-                <div class="tableCol">Title</div>
-                <div class="tableCol">Description</div>
+            <div className="tableWrap tableHeader">
+                <div className="tableCol">Title</div>
+                <div className="tableCol">Description</div>
             </div>
         </div>
       {props && props.data.map(card => (
