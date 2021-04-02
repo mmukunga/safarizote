1b781c285bfe6e941db8e1baf560062808b00d29
Simon Mukunga <mmukunga@gmail.com>
2021-04-02 23:50:11 +0200

    first commit

--- webapp/src/pages/Stock.jsx
+++ webapp/src/pages/Stock.jsx
@@ -98,6 +98,9 @@ const Stock = () => {
     }, []);
 
     const Quote = ({name, list}) => {
+      console.log('1..items..');
+      console.log(list);
+      console.log('2..items..');
       return (
         <div className="QuoteContainer">
           <p>{name}</p>
