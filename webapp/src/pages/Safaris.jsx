fb79c46564e6a755ee8efed5fc1d344f2be1ad37
Simon Mukunga <mmukunga@gmail.com>
2021-04-03 21:01:09 +0200

    first commit

--- webapp/src/pages/Safaris.jsx
+++ webapp/src/pages/Safaris.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import axios from 'axios';
-import Table from './Table';
 import Card from './Card';
 import Tableless from './Tableless';
 
