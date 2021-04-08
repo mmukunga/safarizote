b9402a75ffc84549dcd04bb8ab1bd2e4253484e2
Simon Mukunga <mmukunga@gmail.com>
2021-04-09 00:09:12 +0200

    first commit

--- webapp/src/pages/Tableless.jsx
+++ webapp/src/pages/Tableless.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import { Link } from "react-router-dom";
 import parse from "html-react-parser";
 
@@ -14,7 +14,7 @@ const Tableless = props => {
         )
     }
 
-    const Accordion = ({ children, title, isExpand = false, description }) => {
+    const Accordion = ({ children, title, isExpand = false }) => {
       const [expand, setExpand] = useState(isExpand);
       return (
         <div className="box">
@@ -36,17 +36,18 @@ const Tableless = props => {
           <div className="tableCol tableHeader">Description</div>
         </div>
       </div>
-      {props && props.data.map(card => { 
-        return (
-          <div style={{ margin: '1em' }}>  
-            <Card {...card} />
-            <Accordion isExpand={true} title={card.title} description={card.description}>
-              {Number.isInteger(props.description) ? props.description : parse(props.description)}
-              <Link to={{ pathname: '/email', state: `${props.title}` }} style={{background:'#f8f9fa', textDecoration:'none', fontWeight: 'bold', color: 'brown'}}>Please Click here to enquire</Link>
-            </Accordion>
-          </div>
-        )}
-      )}
+      {props && props.data.map(card =>{ return (
+        <>
+        <Card {...card} />
+        <Accordion isExpand={true} title={card.title}>
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+          The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content 
+          here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use 
+          Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+          Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </Accordion>
+        </>
+      ); })}
     </div>
   );
 } 
