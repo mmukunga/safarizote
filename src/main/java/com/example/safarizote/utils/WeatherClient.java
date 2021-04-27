a341d4b090f7176797979d5ed2ebfad9ac644805
Simon Mukunga <mmukunga@gmail.com>
2021-04-27 23:14:48 +0200

    first commit

--- src/main/java/com/example/safarizote/utils/WeatherClient.java
+++ src/main/java/com/example/safarizote/utils/WeatherClient.java
@@ -4,6 +4,11 @@ import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import java.net.URL;
 import java.util.Scanner;
 
