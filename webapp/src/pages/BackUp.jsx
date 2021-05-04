8c6a8f0e654375c8f90a852bbd3c7c4d0e529d57
Simon Mukunga <mmukunga@gmail.com>
2021-05-05 00:20:44 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -30,7 +30,7 @@ import axios from 'axios';
 
     const uncheckAll = () => {
       alert('!!UNCHECK!!');
-      const categoryTemp = {...category};
+      var categoryTemp = {...category};
       console.log(categoryTemp);
       categoryTemp.isChecked = false;
       categoryTemp.children.map((cat) => {
