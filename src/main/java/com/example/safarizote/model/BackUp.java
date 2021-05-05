1944a604cfcb9a7d962ab88c88a525563a0c75c7
Simon Mukunga <mmukunga@gmail.com>
2021-05-06 01:19:50 +0200

    first commit

--- src/main/java/com/example/safarizote/model/BackUp.java
+++ src/main/java/com/example/safarizote/model/BackUp.java
@@ -12,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
