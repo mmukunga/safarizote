a3b31cb4f92debfa048ddbf019eb270b253de687
Simon Mukunga <mmukunga@gmail.com>
2021-05-14 21:46:32 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -4,23 +4,6 @@ import axios from 'axios';
 import CheckboxTree from 'react-checkbox-tree';
 import 'react-checkbox-tree/lib/react-checkbox-tree.css';
 
-  const parents = [];
-  for (let i = 0; i < 3; i += 1) {
-    const children = [];
-    for (let j = 0; j < 2; j += 1) {
-      children.push({
-        value: `node-0-${i}-${j}`,
-        label: `Node 0-${i}-${j}`,
-      });
-    }
-
-    parents.push({
-      value: `node-0-${i}`,
-      label: `Node 0-${i}`,
-      children,
-    });  
-  }
-
  
   const BackUp = () => {
     const [category, setCategory] = React.useState([]);  
@@ -29,71 +12,90 @@ import 'react-checkbox-tree/lib/react-checkbox-tree.css';
     const [nodes, setNodes] = React.useState([{
       value: 'node-0',
       label: 'Node 0',
-      children: parents,
+      children: createParents(category),
     }]);
 
-
-    const createTree = (obj) => {
-      let initArr = {
-        id: obj.id, 
-        name: obj.name, 
-        isChecked: obj.isChecked, 
-        dateCreated: obj.dateCreated, 
-        children: obj.children
-      };
-
-      const getChildren = (ar) => {
-        ar.children.forEach(val => {
-          if (Array.isArray(val.children) && val.children.length > 0) {
-            getChildren(val);
-          }
-        })
-        return initArr;
+  const createParents = (obj) => {   
+    console.log(obj);
+    const parents = [];
+    for (let i = 0; i < 3; i += 1) {
+      const children = [];
+      for (let j = 0; j < 2; j += 1) {
+        children.push({
+          value: `node-0-${i}-${j}`,
+          label: `Node 0-${i}-${j}`,
+        });
       }
-      return getChildren(initArr);
-    }
 
-    React.useEffect(() => {
-      axios.get("/api/categories").then(response => {
-          setCategory(response.data);
-      }).catch(error => {
-          console.log(error);
-      });
-    }, []);  
-
-    const onCheck = (checked) => {
-      setTreeState({ ...treeState, checked: checked });
+      parents.push({
+        value: `node-0-${i}`,
+        label: `Node 0-${i}`,
+        children,
+      });  
     }
-    
-    const onExpand = (expanded) => {
-      setTreeState({...treeState, expanded: expanded });
-    }
+  }
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log("Submited OK!!");
+  const createTree = (obj) => {
+    let initArr = {
+      id: obj.id, 
+      name: obj.name, 
+      isChecked: obj.isChecked, 
+      dateCreated: obj.dateCreated, 
+      children: obj.children
     };
 
-    const { checked, expanded } = treeState;
+    const getChildren = (ar) => {
+      ar.children.forEach(val => {
+        if (Array.isArray(val.children) && val.children.length > 0) {
+          getChildren(val);
+        }
+      })
+      return initArr;
+    }
+    return getChildren(initArr);
+  }
 
-    return (
-      <Card className="InnerCard" fontColor="black">
-        <strong>Tree BackUp</strong>
-        <h3>Check and Uncheck</h3>
-        <form onSubmit={handleSubmit}>
-          <div className="BackUps">
-            <CheckboxTree
-                checked={checked}
-                expanded={expanded}
-                iconsClass="fa5"
-                nodes={nodes}
-                onCheck={onCheck}
-                onExpand={onExpand}
-            />
-          </div>  
-        </form>
-      </Card>
-    );
+  React.useEffect(() => {
+    axios.get("/api/categories").then(response => {
+        setCategory(response.data);
+    }).catch(error => {
+        console.log(error);
+    });
+  }, []);  
+
+  const onCheck = (checked) => {
+    setTreeState({ ...treeState, checked: checked });
+  }
+    
+  const onExpand = (expanded) => {
+    setTreeState({...treeState, expanded: expanded });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submited OK!!");
+  };
+
+  const { checked, expanded } = treeState;
+
+  return (
+    <Card className="InnerCard" fontColor="black">
+      <strong>Tree BackUp</strong>
+      <h3>Check and Uncheck</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="BackUps">
+          <CheckboxTree
+              checked={checked}
+              expanded={expanded}
+              iconsClass="fa5"
+              nodes={nodes}
+              onCheck={onCheck}
+              onExpand={onExpand}
+          />
+        </div>  
+      </form>
+    </Card>
+  );
   }
 
   export default BackUp;
