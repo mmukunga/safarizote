e1b84c8856d1eb33607241a9a466b0aa91939a29
Simon Mukunga <mmukunga@gmail.com>
2021-05-19 17:48:54 +0200

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -103,7 +103,7 @@ public class BackUpController {
             System.out.println("1...Start....");
             Set<BackUp> childs = items.get(i).getChildren();           
             for(BackUp child : childs){
-                System.out.println(child + " : " + targetFolder.getId());
+                System.out.println(child + " <:> " + targetFolder.getId());
                 if(child.getId() == targetFolder.getId()){
                     parent = items.get(i);
                 }
