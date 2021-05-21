ca31f15a1ed610930697baeccbf7275c43940f98
Simon Mukunga <mmukunga@gmail.com>
2021-05-21 23:09:15 +0200

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -94,16 +94,34 @@ public class BackUpController {
                 }
             }
         }
+        
+        Path sourceDir;
+        Path targetDir;
 
-        String sourceDir = parent.getName();
-        sourceDir = sourceDir.replaceAll(":", ":/");
-        System.out.println("BackUpController: SourceDir:= " + sourceDir);
-        for (BackUp target : targets) {
-            String targetDir = target.getName();
-            targetDir = targetDir.replaceAll(":", ":/");
-            System.out.println("BackUpController: TargetId:= " + target.getId() + " TargetName:= " + targetDir);
-            System.out.println("BackUpController: TargetDir:= " + targetDir);
-            //Files.walkFileTree(sourceDir, new CopyDir(sourceDir, targetDir));
+        String source = parent.getName();
+        source = source.replaceAll(":", ":/");
+        for (BackUp backUp : targets) {
+            String target = backUp.getName();
+            target = target.replaceAll(":", ":/");
+            System.out.println("--Start---");
+                System.out.println("BackUpController: Source:= " + source);
+                System.out.println("BackUpController: Target:= " + target);
+
+                if (!osName.contains("Linux")) {
+                    sourceDir = Paths.get(backUp.getName());
+                    targetDir = Paths.get(target.getName());
+                } else {
+                    int index = source.indexOf(":/");
+                    System.out.println(source.substring(index+1));    
+                    sourceDir = Paths.get("/home/x00sms/source/".concat(source.substring(index+1)));
+                    index = target.indexOf(":/");
+                    System.out.println(target.substring(index+1)); 
+                    targetDir = Paths.get("/home/x00sms/target/".concat(target.substring(index+1)));
+                }
+                System.out.println("BackUpController: SourceDir:= " + sourceDir);
+                System.out.println("BackUpController: TargetDir:= " + targetDir);
+                //Files.walkFileTree(sourceDir, new CopyDir(sourceDir, targetDir));
+            System.out.println("--End---");
         }    
         
         logger.warn("BackUp Completed OK!");
