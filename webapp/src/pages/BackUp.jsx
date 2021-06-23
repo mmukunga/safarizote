67b1c82b1b66124d1c92e5bda50cea6d183f0681
Simon Mukunga <mmukunga@gmail.com>
2021-06-23 20:31:54 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -70,7 +70,7 @@ import 'react-checkbox-tree/lib/react-checkbox-tree.css';
         var formData = new FormData();
         formData.append("file", document.forms["userForm"].file.files[0]);
         axios.post('/api/uploadFile', formData, {   
-          headers : { 'Content-type' : 'application/json', Accept : 'application/json'}
+          headers : { 'Content-type' : 'multipart/form-data', Accept : 'application/json'}
         }).then(response => {
             console.log(response);
         }).catch(error => {
