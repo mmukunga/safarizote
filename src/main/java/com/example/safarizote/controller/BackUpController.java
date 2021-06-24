1925f8f9b9a973f1a1d05a9510de14c11c54c3c5
Simon Mukunga <mmukunga@gmail.com>
2021-06-24 07:49:27 +0200

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -33,6 +33,7 @@ import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
