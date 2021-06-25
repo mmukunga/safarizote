9a8036492d6dcb8daebbaf9165fee4a3a06def18
Simon Mukunga <mmukunga@gmail.com>
2021-06-26 01:41:47 +0200

    first commit

--- src/main/java/com/example/safarizote/SafarizoteApplication.java
+++ src/main/java/com/example/safarizote/SafarizoteApplication.java
@@ -3,6 +3,8 @@ package com.example.safarizote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
