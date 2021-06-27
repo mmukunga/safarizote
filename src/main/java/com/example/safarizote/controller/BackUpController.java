3854d64aff54675fe8200923e3239be22dcdd051
Simon Mukunga <mmukunga@gmail.com>
2021-06-27 12:10:10 +0200

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -74,7 +74,7 @@ public class BackUpController {
     }
 
     @RequestMapping(value="/api/doCloud", method={RequestMethod.POST})
-    public ResponseEntity<Object> doCloud(@RequestBody List<BackUp> folders) throws Exception {
+    public ResponseEntity<Object> doCloud(@RequestBody BackUp file) throws Exception {
         System.out.println("doCloud");
         System.out.println(file);
         if (file.isEmpty()) {
