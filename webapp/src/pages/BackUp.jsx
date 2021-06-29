2da703c1df9538bf8cb9602718bf3f760141b036
Simon Mukunga <mmukunga@gmail.com>
2021-06-29 19:03:15 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -148,7 +148,14 @@ const reducer = (state, action) => {
      
       sendData().then((result) => {
         console.log(result);
-        result.data.append("ASadasdsdweedafsfsdf"); 
+        const nonExisting = {
+          id: 10000,
+          content: 'This note is not saved to server',
+          date: '2019-05-30T17:30:31.098Z',
+          important: true,
+        };
+
+        result.data.concat(nonExisting);
         console.log(result);
         const options = {
           headers: {
