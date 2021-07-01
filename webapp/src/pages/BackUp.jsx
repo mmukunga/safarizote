43a99b7f40c623cf3004a28febf587b1046a5085
Simon Mukunga <mmukunga@gmail.com>
2021-07-01 07:09:57 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -103,7 +103,9 @@ const reducer = (state, action) => {
             }
           };
           
-          axios.post("/api/uploadFile", result, options).then((response) => { 
+          let formData = new FormData();
+          formData.append('file', state.file, "filename");
+          axios.post("/api/uploadFile", formData, options).then((response) => { 
               console.log(response);
           }).catch(error => {
               console.log(error);
