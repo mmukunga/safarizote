151e2c5976119f6b927640117bb322c33d4e5a26
Simon Mukunga <mmukunga@gmail.com>
2021-07-14 18:50:59 +0200

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -23,7 +23,7 @@ import java.net.URL;
 import com.google.cloud.ReadChannel;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.Blob;
-import com.google.cloud.ReadChannel;
+import com.google.cloud.Channels;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.StorageOptions;
 import com.google.cloud.storage.Blob.BlobSourceOption;
