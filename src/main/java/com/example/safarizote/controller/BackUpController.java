86c385139fa212ab69ebe3a4255b9c0124d123f0
Simon Mukunga <mmukunga@gmail.com>
2021-07-14 12:10:36 +0200

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -161,7 +161,7 @@ public class BackUpController {
         // [END storage_download_file]
         byte[] content = blob.getContent(BlobSourceOption.generationMatch());
         String s = new String(content);
-        byte[] encodedBytes = Base64.getDecoder().decode(content);
+        byte[] encodedBytes = Base64.getDecoder().decode(content, Base64.NO_WRAP);
         String fileContent2 = new String(encodedBytes);
         System.out.println("fileContent from GoogleCloud Storage fileContent2:= " + fileContent2);
 
