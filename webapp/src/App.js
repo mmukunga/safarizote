9517a72fe98dcf7393035c4b5ff7dafd4e17f89d
Simon Mukunga <mmukunga@gmail.com>
2021-07-28 16:26:51 +0200

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -95,7 +95,7 @@ function App() {
                 <NavLink to="/aboutUs" className="Nav_link">About&nbsp;Us</NavLink>
                 <NavLink to="/signIn" className="Nav_link">Login</NavLink>
                 <Menu/>
-                <button onClick={handleLogout} className="LogOut">Logout</button>
+                <button onClick={handleLogout} className="LogOut"></button>
               </div>
             </nav> 
             <p className="App-title">
