9f872b5955b64b2b0a575ce32a0429911658c653
Simon Mukunga <mmukunga@gmail.com>
2021-08-03 01:06:19 +0200

    first commit

--- webapp/src/pages/Safaris.jsx
+++ webapp/src/pages/Safaris.jsx
@@ -45,9 +45,10 @@ const Safaris = () => {
       axios.get('https://ipinfo.io/json').then(response => {
             console.log(response.data);
            // http://api.ipstack.com/84.212.216.80?access_key=52422dae81a62d23abbbeca1498b494a
-            axios.get(`https://ipapi.co/json/?q=${response.data.ip}?access_key=${key}`)
+           //  axios.get(`https://ipapi.co/json/?q=${response.data.ip}?access_key=${key}`)
+           axios.get(`https://ipapi.co/json/${response.data.ip}?access_key=${key}`)
             .then(resp => {
-              console.log(resp.data);
+              console.log(resp);
             });
         }).catch(e => {
             console.log(e);
