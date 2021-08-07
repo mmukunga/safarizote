3b12c229e678a69d54c2fa63ca25c38a9353cbe6
Simon Mukunga <mmukunga@gmail.com>
2021-08-07 14:19:14 +0200

    first commit

--- src/main/java/com/example/safarizote/repository/MetricsLoader.java
+++ src/main/java/com/example/safarizote/repository/MetricsLoader.java
@@ -25,11 +25,37 @@ public class MetricsLoader implements CommandLineRunner {
             return;
         }
         
-        repository.save(Metrics.builder().url("www.wanja.co.ke").browser("Safari").dateCreated(Instant.now()).build());
-        repository.save(Metrics.builder().url("http://kiwani.com").browser("Microsft Explorer Edge").dateCreated(Instant.now()).build());
-        repository.save(Metrics.builder().url("https://mahi.org").browser("Mozilla Firefox").dateCreated(Instant.now()).build());
-        repository.save(Metrics.builder().url("207.51-175-209.customer.lyse.net").browser("Android").dateCreated(Instant.now()).build());
-        repository.save(Metrics.builder().url("https://tulinambo.congo.env").browser("TikTok").dateCreated(Instant.now()).build());
+        repository.save(Metrics.builder().url("www.wanja.co.ke").browser("Safari")
+        .city("Oslo")
+        .organization("Telia Norge AS") 
+        .connectionType("Simon Test3")
+        .continent("Cable/DSL")
+        .continentCode("Get AS")
+        .country("Norway")
+        .countryCode("NO")
+        .currencyName("Norwegian Krone")
+        .currencyCode("NOK")
+        .emoji("NO")
+        .flagPng("https://static.abstractapi.com/country-flags/NO_flag.png")
+        .flagSvg("https://static.abstractapi.com/country-flags/NO_flag.svn")
+        .ipAddress("84.212.216.80")
+        .latitude("59.955")
+        .longitude("10.859")
+        .postalCode("0458")
+        .region("Oslo County")
+        .regionIsoCode("03")
+        .timezoneName("Europe/Oslo")
+        .timezoneAbbreviation("CEST")
+        .currentTime("11:30:31")
+        .browserName("Chrome")
+        .browserVersion("92.0.4515.131")
+        .browserOsName("Windows")
+        .browserOsVersion("NT 10.0")
+        .dateCreated(Instant.now()).build());
+        //repository.save(Metrics.builder().url("http://kiwani.com").browser("Microsft Explorer Edge").dateCreated(Instant.now()).build());
+        //repository.save(Metrics.builder().url("https://mahi.org").browser("Mozilla Firefox").dateCreated(Instant.now()).build());
+        //repository.save(Metrics.builder().url("207.51-175-209.customer.lyse.net").browser("Android").dateCreated(Instant.now()).build());
+        //repository.save(Metrics.builder().url("https://tulinambo.congo.env").browser("TikTok").dateCreated(Instant.now()).build());
 
         repository.findAll().forEach((tracker) -> {
             logger.info("{}", tracker);
