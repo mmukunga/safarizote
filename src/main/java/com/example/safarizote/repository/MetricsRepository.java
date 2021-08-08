1e4fe21698f2267892b897ef115d7dc3fd5e43b6
Simon Mukunga <mmukunga@gmail.com>
2021-08-08 11:37:43 +0200

    first commit

--- src/main/java/com/example/safarizote/repository/MetricsRepository.java
+++ src/main/java/com/example/safarizote/repository/MetricsRepository.java
@@ -1,6 +1,7 @@
 package com.example.safarizote.repository;
 
 import java.util.Set;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
