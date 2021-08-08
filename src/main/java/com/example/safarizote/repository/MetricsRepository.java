caa8c1fcc8873f8b07eb12c57aff2bbd409d7ab0
Simon Mukunga <mmukunga@gmail.com>
2021-08-08 14:35:05 +0200

    first commit

--- src/main/java/com/example/safarizote/repository/MetricsRepository.java
+++ src/main/java/com/example/safarizote/repository/MetricsRepository.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.safarizote.model.Metrics;
 
 @Repository
 public interface MetricsRepository extends JpaRepository<Metrics, Long> {
     Set<Metrics> findByUrl(String url);
+    @Transactional
     List<Metrics> deleteByUrl(String url);
 }
