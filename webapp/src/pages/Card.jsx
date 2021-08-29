a8c6ff483f10f785def90c89f2c7240e267edf2a
Simon Mukunga <mmukunga@gmail.com>
2021-08-29 09:36:11 +0200

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -1,4 +1,5 @@
-import React from 'react';;
+import React from 'react';
+import styled, { css } from 'styled-components';
 
 const Card = (props) => {
   const { className, fontColor, children } = props;
