6877bac2f8bf06a5f3c7b0178694a2091eff06fc
Simon Mukunga <mmukunga@gmail.com>
2021-08-29 22:38:02 +0200

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -2,70 +2,21 @@ import React from 'react';
 
 const Card = (props) => {
   const { className, fontColor, children } = props;
-  var font_size;
-
-  const minStyles = {
-    width: 200,
-    height: 50,
-    backgroundColor: "red"
-  };
-  
-  const maxStyles = {
-    width: 300,
-    height: 50,
-    backgroundColor: "blue"
-  };
-
-  const renderStyles = () => {
-    if (window.innerWidth <= 800) {
-      return minStyles;
-    }
-    return maxStyles;
-  };
-
-  const divStyle = {
-      width: "800px",
-      backgroundColor: "purple",
-      margin: "auto",
-      "@media (max-width: 600px)": {
-          width:  "400px",
-          backgroundColor: "green",
-          margin: "auto",
-          fontSize: "24px"
-      }
-  };
-
-  const styles = {
-    hideThisOnBigScreens: {
-      display: 'block',
-      border: '2px solid purple',
-      '@media screen and (max-width: 600px)': {
-          display: 'inline-block'
-      }
-    }
-  };
-
   const labelStyles = {
     fontSize: '2.5vw',
     margin: '2px',
     padding: '2px',
-    border: '2px solid red'
+    border: '2px solid red',
+    '@media (max-width: 600px)': {
+        backgroundColor: 'green',
+        fontSize: '24px'
+    }
   };
 
   return (
     <div className={className}>
       <div style={labelStyles}>
         {children}
-        <div style={renderStyles()}>
-          One of the jobs of developers is to try to find the most performant 
-          solution that won't take a lot of time to implement and also help 
-          build a strong, resilient application in the long-term.
-        </div>
-        <div style={divStyle}>
-          In React, inline styles are not specified as a string. Instead they 
-          are specified with an object whose key is the camelCased version of 
-          the style name, and whose value is the style’s value, usually a string.
-        </div>
       </div>
     </div>
   )
