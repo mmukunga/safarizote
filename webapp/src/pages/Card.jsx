c74424976d9d4c71a330ceb324e793653fadabee
Simon Mukunga <mmukunga@gmail.com>
2021-08-29 20:23:12 +0200

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -2,6 +2,37 @@ import React from 'react';
 
 const Card = (props) => {
   const { className, fontColor, children } = props;
+  var font_size;
+
+  const minStyles = {
+    width: 200,
+    height: 50,
+    backgroundColor: "red"
+  };
+  
+  const maxStyles = {
+    width: 300,
+    height: 50,
+    backgroundColor: "blue"
+  };
+
+  const renderStyles = () => {
+    if (window.innerWidth <= 800) {
+      return minStyles;
+    }
+    return maxStyles;
+  };
+
+  const styles = {
+    hideThisOnBigScreens: {
+      display: 'block',
+      border: '2px solid purple',
+      '@media screen and (max-width: 600px)': {
+          display: 'inline-block'
+      }
+    }
+  };
+
   const labelStyles = {
     fontSize: '2.5vw',
     margin: '2px',
@@ -13,6 +44,11 @@ const Card = (props) => {
     <div className={className}>
       <div style={labelStyles}>
         {children}
+        <div style={renderStyles()}>
+          One of the jobs of developers is to try to find the most performant 
+          solution that won't take a lot of time to implement and also help 
+          build a strong, resilient application in the long-term.
+        </div>
       </div>
     </div>
   )
