b6b05599794c5db5c0207f8d7dc8e63737396e74
Simon Mukunga <mmukunga@gmail.com>
2021-08-29 10:23:29 +0200

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
 
 const Card = (props) => {
   const { className, fontColor, children } = props;
-  const CardWrapper = styled.div`
-      fontSize: '2.5vw';
-      margin: '2px';
-      padding: '2px';
-      border: '2px solid red';
-
-      @media screen and (max-width: 600px) {
-          font-size: '30px';
-      }
-`;
+  const labelStyles = {
+    fontSize: '2.5vw',
+    margin: '2px',
+    padding: '2px',
+    border: '2px solid red'
+  };
 
   return (
     <div className={className}>
-        <CardWrapper>
-            {children}
-        </CardWrapper>
+      <div style={labelStyles}>
+        {children}
+      </div>
     </div>
   )
 }
 
 export default Card;
+
