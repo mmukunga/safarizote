8204fbaace876f90f185d1c88ef6d177c84aeb27
Simon Mukunga <mmukunga@gmail.com>
2021-08-30 16:57:36 +0200

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -5,17 +5,17 @@ const Card = (props) => {
   const { className, fontColor, children } = props;
   const labelStyles = {
     divStyle: {
-      fontSize: '2.5vw',
+      fontSize: '2.0vw',
       margin: '2px',
       padding: '2px',
       border: '2px solid red',
       '@media (max-width: 600px)': {
           backgroundColor: 'green',
-          fontSize: '24px'
+          fontSize: '18px'
       }
     }
   };
-  
+
   const StyleRoot = Radium.StyleRoot;
 
   return (
