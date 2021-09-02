2fcc994abe6c9a62be222862309284ac295081aa
Simon Mukunga <mmukunga@gmail.com>
2021-09-02 17:11:04 +0200

    first commit

--- src/main/java/com/example/safarizote/controller/BackUpController.java
+++ src/main/java/com/example/safarizote/controller/BackUpController.java
@@ -100,7 +100,7 @@ public class BackUpController {
       // Get specific file from specified bucket
       Storage storage = StorageOptions.newBuilder().setProjectId(PROJECT_ID).setCredentials(credentials).build().getService();
       List<URL> imageUrls = new ArrayList<>();
-      String directoryPrefix = "2017 Olaug";
+      String directoryPrefix = "Olaug";
 
       Page<Blob> listObjects = storage.list(BUCKET_NAME,
             Storage.BlobListOption.prefix(directoryPrefix),
