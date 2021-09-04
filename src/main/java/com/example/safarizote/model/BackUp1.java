3e7d56e364beff50111525735b6cfd2eea146fcc
Simon Mukunga <mmukunga@gmail.com>
2021-09-04 08:54:02 +0200

    first commit

--- src/main/java/com/example/safarizote/model/BackUp1.java
+++ src/main/java/com/example/safarizote/model/BackUp1.java
@@ -1,12 +1,7 @@
 package com.example.safarizote.model;
 
-import java.time.Instant;
-import javax.persistence.OneToMany;
-import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
