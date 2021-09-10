13c21e7823b7dd145d160cb8105267ebfd93b3e1
Simon Mukunga <mmukunga@gmail.com>
2021-09-10 14:35:41 +0200

    first commit

--- webapp/src/pages/Private.jsx
+++ webapp/src/pages/Private.jsx
@@ -7,26 +7,30 @@ const Private = ({ component: Component, ...rest }) => {
   const userAuth = localStorage.getItem('userAuth');
   console.log('2.PrivateRoute...');
   console.log(userAuth);
-const isValidUser = () => {
-  console.log('..isValidUser..');
-  if (localStorage.getItem('userAuth') !== null) {
-    const userAuth = localStorage.getItem('userAuth');
-    console.log(userAuth);
-    axios.post('/api/verify', {
-        token: userAuth.token,
-        username: userAuth.username
-    }).then(response => {
-        console.log(response);
-        return true;
-    }).catch(error => {
-        console.log(error);
-        return false;
-    });
+  const isValidUser = () => {
+    console.log('..isValidUser..');
+    if (localStorage.getItem('userAuth') !== null) {
+      const userAuth = localStorage.getItem('userAuth');
+      console.log(userAuth);
+      axios.post('/api/verify', {
+          token: userAuth.token,
+          username: userAuth.username
+      }).then(response => {
+          console.log(response);
+          return true;
+      }).catch(error => {
+          console.log(error);
+          return false;
+      });
+    }
+    console.log('..isValidUser.. END!!');
+    return false;
   }
-  console.log('..isValidUser.. END!!');
-  return false;
-}
 
+  console.log('20.PrivateRoute...');
+  console.log(isValidUser);
+  console.log('30.PrivateRoute...');
+  
   return (
     <Route
       {...rest}
