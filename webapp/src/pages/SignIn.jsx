6b02da3f0c07c4e9e690284b5da803f07047828d
Simon Mukunga <mmukunga@gmail.com>
2021-09-12 01:32:08 +0200

    first commit

--- webapp/src/pages/SignIn.jsx
+++ webapp/src/pages/SignIn.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import {Redirect, useHistory, useLocation } from 'react-router-dom';
 import Card from './Card';
 import UserService from './UserService';
 
