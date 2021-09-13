8a8836060dba3a5d1e230561d2db1759eb88a7e4
Simon Mukunga <mmukunga@gmail.com>
2021-09-13 17:57:22 +0200

    first commit

--- webapp/src/pages/UserService.jsx
+++ webapp/src/pages/UserService.jsx
@@ -2,7 +2,6 @@
 const isLoggedIn = localStorage.getItem('access_token') !== null;
 
 const loginUser = async (user) => {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
   const { username, password } = user;
   if (username === 'm@gmail.com' && password === '12345') {
