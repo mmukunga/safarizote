961c602f90b68f9ae313a2625b75bfdc967bb8a1
Simon Mukunga <mmukunga@gmail.com>
2021-09-16 22:45:06 +0200

    first commit

--- webapp/src/pages/UserService.jsx
+++ webapp/src/pages/UserService.jsx
@@ -1,3 +1,4 @@
+import {default as UUID} from "node-uuid";
 
 const isLoggedIn = () => {
   localStorage.setItem('jwt_token', 'access_token');
@@ -5,16 +6,30 @@ const isLoggedIn = () => {
 }
 
 const loginUser = async (user) => {
-  const { username, password } = user;
+   const { username, password } = user;
 
-  console.log('!!Deploy a GitHub branch!! ' + localStorage.getItem('jwt_token'));
-  if (username === 'm@gmail.com' && password === '12345') {
-    console.log('!!AUTHENTICATED!!');
-    localStorage.setItem('jwt_token', 'access_token');
-    return { token: 'access_token' };
-  } else {
-    throw new Error('Wrong username or password');
-  }
+   console.log('!!Deploy a GitHub branch!! ' + localStorage.getItem('jwt_token'));
+
+   const userAuth = {
+        id: UUID.v4(), 
+        email: 'm@gmail.com',
+        password: '12345',
+        token: '',
+        dateCreated: new Date()
+   }
+   const axiosTest = () => {
+      return axios.post('/api/login', userAuth).then(response => {
+          console.log('1.!!AUTHENTICATED!!');
+          console.log(response);
+          console.log('2.!!AUTHENTICATED!!');
+          localStorage.setItem('jwt_token', 'access_token');
+          return { token: 'access_token' };
+      }).catch(error => {
+          console.error('There was an error!', error);
+          throw new Error('Wrong username or password');
+      });
+   } 
+   return axiosTest;
 }
 
 const logOut = () => {
