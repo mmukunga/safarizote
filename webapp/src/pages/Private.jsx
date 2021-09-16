f281162485e259c240ab9ad5d28085a7ee369a52
Simon Mukunga <mmukunga@gmail.com>
2021-09-16 18:16:38 +0200

    first commit

--- webapp/src/pages/Private.jsx
+++ webapp/src/pages/Private.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import axios from 'axios';
 import { Redirect, Route } from 'react-router-dom';
 import UserService from './UserService';
 
 const Private = ({component: Component, ...rest}) => {
-  const loggedIn =  UserService.isLoggedIn();
   console.log('UserService.isLoggedIn? ' + UserService.isLoggedIn());
   return (
     <Route {...rest} render={props => (            
-      loggedIn == 'access_token'
+      UserService.isLoggedIn() == 'access_token'
         ? <Component {...props} />            
         : <Redirect  to={{pathname: '/signIn', state: {from: props.location}}} />        
     )} />   
