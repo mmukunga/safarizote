74d9e7dfbb9ffc771ef2ab58c58932605c1b8b2a
Simon Mukunga <mmukunga@gmail.com>
2021-09-20 19:43:00 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -83,6 +83,9 @@ const BackUp = () => {
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <p>WEKA HII to my backUp!!</p>
+      <img src="https://storage.googleapis.com/sms_familie_album/FrognerParken_Samuel.pdf?GoogleAccessId=safarizote-service-account@familiealbum-sms.iam.gserviceaccount.com&Expires=1650155798&Signature=Doh0Fcgsc1e9JxRFiAzdPTJhkAVsZcNPi6mSj43ugaa01D5hlPfGSxdSn%2BBjh2UIXycuGfVA5T4BoIck%2Fi3ZaK670Q%2FT%2FcEsb1h0OGbBszkVCVCVV%2BMB6FEUdhi0vH5a49i%2BHs9%2FCAlbmIxI3V94QiUCtaOCLMZEbI35%2F67MQ0n%2FTXpattkFzrqR688aFjXXYlBODhpZ8cM%2B6iTxJe5eF%2BSCeglkPqb3SuDQJxVxXrN9sJ%2BCKMnvXHJMvVrabrLo3DNDySkc82ayivwHjcM5A8EjWOEQYgL5NZ2daHBEGo6beA22K4SkjkwNawFQ1R1cluL%2BwGQAF%2F6G6vKIV%2FYjkA%3D%3D"
+           alt="Samuel på Frogner parken"/>
       <p>Welcome to my backUp!!</p>
       {data}
       <div className="userList">
