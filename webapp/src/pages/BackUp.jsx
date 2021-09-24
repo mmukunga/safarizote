a788d8209eb40a56f77474ddafbfa7be7e286f8e
Simon Mukunga <mmukunga@gmail.com>
2021-09-24 21:11:12 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import ReactPaginate from 'react-paginate';
  
@@ -31,9 +31,7 @@ function BackUp() {
     setPageCount(Math.ceil(data.length / postsPerPage));
   }
 
-
- 
-  useEffect(() => {
+  React.useEffect(() => {
     getAllPosts();
   }, [offset]);
 
