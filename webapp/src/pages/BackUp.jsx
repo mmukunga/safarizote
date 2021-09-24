36a246adeea15433011b47a89009dafab5f5a456
Simon Mukunga <mmukunga@gmail.com>
2021-09-24 08:00:33 +0200

    first commit

--- webapp/src/pages/BackUp.jsx
+++ webapp/src/pages/BackUp.jsx
@@ -1,94 +1,65 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ReactPaginate from 'react-paginate';
-
-
-const BackUp = () => {
-  const [name,  setName]  = useState("");
-  const [value, setValue] = useState('');
-  const [items, setItems] = React.useState([]);
-  const [offset, setOffset] = useState(0);
-  const [data, setData] = useState([]);
-  const [perPage] = useState(10);
-  const [pageCount, setPageCount] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  
-    const getData = async() => {
-      setIsLoading(true);
-      const res = await axios.get(`/api/categories`);
-      const data = res.data;
-
-      const result = data.map(a => a.name);
-      setItems(prevItems => [...prevItems, ...result]);
-
-      const slice = data.slice(offset, offset + perPage);
-      const postData = slice.map((pd,id) => <div key={id} className="Thumbnail">
-          <div className="gallery">
-            <p className="desc">{pd.name}</p>
-            <img src={pd.path} alt=" S M S "/>
-          </div>
-      </div>);
-      setData(postData);
-      setPageCount(Math.ceil(data.length / perPage));
-      setIsLoading(false);
-  }
-
-  const handlePageClick = (e) => {
-      const selectedPage = e.selected;
-      setOffset(selectedPage + 1)
-  };
-
-  useEffect(() => {
-    getData()
-  }, [offset])
-
-  const onChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const handleSubmit = (evt) => {
-      evt.preventDefault();
-      alert(`Submitting Name ${name}`)
-  }
-  return (
-    <div className='BackUps'>
-      {isLoading ? (
-        <div className={isLoading ? "loader" : undefined}><div className="spinner" /></div>
-      ) : (
-        <div>Is Loaded!!</div>
-      )}
-
-      <h3>Folders BackUp</h3>
-      <ReactPaginate
-          previousLabel={"prev"}
-          nextLabel={"next"}
-          breakLabel={"..."}
-          breakClassName={"break-me"}
-          pageCount={pageCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageClick}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"}/>
-      <h4>Select folder</h4>
-      <form onSubmit={handleSubmit}>
-        <select onChange={onChange}>
-          {items.map((item, idx) => (
-            <option key={idx} value={item}>{item}</option>
-          ))}
-        </select>
-        <div>Selected folder: {value}</div>
-        <label>Frirst Name:
-          <input type="text" value={name} onChange={e => setName(e.target.value)}/>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      <p>Welcome to my backUp!!</p>
-      { data }
-      <div class="clearfix"></div>
-    </div>
-  );
+import './App.css'
+ 
+function BackUp() {
+ const [postsPerPage] = useState(5);
+ const [offset, setOffset] = useState(1);
+ const [posts, setAllPosts] = useState([]);
+ const [pageCount, setPageCount] = useState(0)
+ 
+ const getPostData = (data) => {
+   return (
+     data.map(post => <div className="container" key={post.id}>
+       <p>User ID: {post.id}</p>
+       <p>Title: {post.title}</p>
+     </div>)
+   )
+ 
+ }
+ 
+ const getAllPosts = async () => {
+   const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+   const data = res.data;
+   const slice = data.slice(offset - 1 , offset - 1 + postsPerPage)
+ 
+   // For displaying Data
+   const postData = getPostData(slice)
+ 
+   // Using Hooks to set value
+   setAllPosts(postData)
+   setPageCount(Math.ceil(data.length / postsPerPage))
+ }
+ 
+ const handlePageClick = (event) => {
+   const selectedPage = event.selected;
+   setOffset(selectedPage + 1)
+ };
+ 
+ useEffect(() => {
+   getAllPosts()
+ }, [offset])
+ 
+ return (
+   <div className="main-app">
+    
+     {/* Display all the posts */}
+     {posts}
+ 
+     {/* Using React Paginate */}
+     <ReactPaginate
+       previousLabel={"previous"}
+       nextLabel={"next"}
+       breakLabel={"..."}
+       breakClassName={"break-me"}
+       pageCount={pageCount}
+       onPageChange={handlePageClick}
+       containerClassName={"pagination"}
+       subContainerClassName={"pages pagination"}
+       activeClassName={"active"} />
+   </div>
+ );
 }
-
-export default BackUp
+ 
+export default BackUp;
