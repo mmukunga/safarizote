70950c24a3813ebf51ce4aa78150500453921068
Simon Mukunga <mmukunga@gmail.com>
2021-09-25 08:22:10 +0200

    first commit

--- webapp/src/pages/Card.jsx
+++ webapp/src/pages/Card.jsx
@@ -5,13 +5,13 @@ const Card = (props) => {
   const { className, fontColor, children } = props;
   const labelStyles = {
     divStyle: {
-      fontSize: '1.5vh',
+      fontSize: '1.8vh',
       margin: '2px',
       padding: '2px',
       border: '2px solid red',
       '@media (max-width: 600px)': {
           backgroundColor: 'green',
-          fontSize: '1.5vw'
+          fontSize: '1.8vw'
       }
     }
   };
