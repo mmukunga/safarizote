ee6758f23c0d89db9e75c3d516b1ed16d1eae69a
Simon Mukunga <mmukunga@gmail.com>
2021-10-01 14:06:22 +0200

    first commit

--- webapp/src/pages/Safaris.jsx
+++ webapp/src/pages/Safaris.jsx
@@ -138,8 +138,11 @@ const Safaris = () => {
       const mod = idx % 2;
       return (
         <div className="containerX">
-          <div className="floatedX">float float float float float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float floatfloat float.</div>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <div className="floatedX">
+              <VideoPlayer video={video} className="VideoPlayer"/>
+          </div>
+          ICON!! {parse(title)} <span className="icon"><i className={`fa fa-play-circle${!expand ? ' down' : ''}`}></i></span> NOCI!!
+          MMS {expand && <div>{children}</div>} SMSM
       </div>
       )
     }
