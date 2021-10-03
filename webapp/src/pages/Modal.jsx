38f0fbd13532159208c6d80aaaa4da426d0e48e8
Simon Mukunga <mmukunga@gmail.com>
2021-10-03 21:17:59 +0200

    first commit

--- webapp/src/pages/Modal.jsx
+++ webapp/src/pages/Modal.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 const Modal = (props) => {
     console.log('Modal - Click!!!!');
+    console.log(props.title);
+    console.log(props.children);
+    console.log('Modal - Click!!!!');
     if(!props.show) {return null;}
     return (
        <div className={`modal ${props.show ? 'show':''}`} onClick={props.onClose} >
           <div className="modal-content" onClick={e => e.stopPropagation()}>
             <header className="modal-header">
-              <h4 className="modal-title">MMS {props.title}</h4> 
+              <h2 className="modal-title">{props.title}</h2> 
             </header>
             <main className="modal-body">
-                {props.children}
+                Les mer i Aftenposten: Bjørndalen og Hanevold ikke tøffe nok. 
+                Her er listen over de 60 tøffeste idrettene: 1. Boksing 2. Ishockey 3. Amerikansk fotball
+                Les mer i Aftenposten: Bjørndalen og Hanevold ikke tøffe nok. 
+                Her er listen over de 60 tøffeste idrettene: 1. Boksing 2. Ishockey 3. Amerikansk fotball
             </main>
             <footer className="modal-footer">
               <button className="modal-button" onClick={props.onClose}>Close</button>
