ba30b80f338b9b43d852b803d73a6cf61711e790
Simon Mukunga <mmukunga@gmail.com>
2021-10-06 20:48:45 +0200

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route, Switch, NavLink, withRouter, useHistory } from "react-router-dom";
 import {ThemeContext} from "./pages/ThemeContext";
-import SwitchButton from "./pages/Button";
+import SwitchButton from "./pages/SwitchButton";
 import logo from "./logo.svg";
 import "./App.css";
 
