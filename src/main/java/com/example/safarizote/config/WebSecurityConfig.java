acf96e5722908724e02109ee5411f864af520410
Simon Mukunga <mmukunga@gmail.com>
2021-10-08 23:44:07 +0200

    first commit

--- src/main/java/com/example/safarizote/config/WebSecurityConfig.java
+++ src/main/java/com/example/safarizote/config/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.safarizote.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
