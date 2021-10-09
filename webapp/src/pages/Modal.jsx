d24d50cbb18425d0e313c8f81502e7a15882e588
Simon Mukunga <mmukunga@gmail.com>
2021-10-09 19:28:29 +0200

    first commit

--- webapp/src/pages/Modal.jsx
+++ webapp/src/pages/Modal.jsx
@@ -4,13 +4,13 @@ import {Link } from "react-router-dom";
 
 const Modal = ({ children, show, setShow}) => {
   const modal = show && (
-      <div className="model">
-        <div className="model-content">
-          <input type="button" value="X" className="close-icon" onClick={() => setShow(false)}/>
-          <div dangerouslySetInnerHTML={{__html: children}} /> 
+      <div className="modalOverlay">
+        <div className="model">
+             <input type="button" value="X" className="close-icon" onClick={() => setShow(false)}/>
+             <div dangerouslySetInnerHTML={{__html: children}} /> 
              <p style={{fontSize:'24px'}}>             
-              <Link to='/email'>You can book Here </Link> &nbsp; Or &nbsp; 
-              <Link to={{ pathname: "/email", state: { modal: true }, }} className="link">Here</Link>
+                <Link to='/email'>You can book Here </Link> &nbsp; Or &nbsp; 
+                <Link to={{ pathname: "/email", state: { modal: true }, }} className="link">Here</Link>
              </p>  
         </div>
       </div>
