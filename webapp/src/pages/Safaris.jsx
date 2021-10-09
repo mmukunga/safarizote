fd793b11f8872a76f57d1ef9dcffa045d7b7703f
Simon Mukunga <mmukunga@gmail.com>
2021-10-09 12:15:50 +0200

    first commit

--- webapp/src/pages/Safaris.jsx
+++ webapp/src/pages/Safaris.jsx
@@ -180,7 +180,7 @@ const Safaris = () => {
     }
     
     return (
-        <video ref={videoRef} controls autoPlay muted className="video-player">
+        <video ref={videoRef} controls loop muted className="video-player">
           {/* Of course it's the big buck bunny! */}
           <source src={props.video} type="video/mp4"/>
         </video>
