f53037f2cb220d3986d553850af7e62291c2e0cf
Simon Mukunga <mmukunga@gmail.com>
2021-10-12 17:46:46 +0200

    first commit

--- webapp/src/pages/PopUp.jsx
+++ webapp/src/pages/PopUp.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
 const PopUp = (props) => {
-  if (!props.open) return null;
+  if (!props.open) { 
+      return null;
+  }
 
   return ReactDOM.createPortal(
     <div className='overlay'>
       <div className='popUp'>
-        <p> Hello World! </p>
-        <button onClick={props.handleClose} > Close Me! </button>
+        <p>Hello World!</p>
+        <button onClick={props.handleClose}>Close Me!</button>
       </div>
     </div>,
     document.getElementById('portal')
-  )
+  );
 }
 
 export default PopUp;
