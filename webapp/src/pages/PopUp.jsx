ddb5427cfdf67572cd32ef87f1e97e9ecbc8a7f1
Simon Mukunga <mmukunga@gmail.com>
2021-10-13 17:15:18 +0200

    first commit

--- webapp/src/pages/PopUp.jsx
+++ webapp/src/pages/PopUp.jsx
@@ -9,6 +9,9 @@ const PopUp = (props) => {
   return ReactDOM.createPortal(
     <div className='overlay'>
       <div className='popUp'>
+        <p align="right">
+          <input type="button" value="Click Me"/>
+        </p>
         <input type="button" value="x" className="close-icon" onClick={props.handleClose}/>
         <div class="popUp-content">
            <div dangerouslySetInnerHTML={{__html: props.children}} /> 
