f018b176864b969a67ab2e5a08c7ba8b0a7b4992
Simon Mukunga <mmukunga@gmail.com>
2021-10-21 17:44:08 +0200

    first commit

--- webapp/src/pages/PopUp.jsx
+++ webapp/src/pages/PopUp.jsx
@@ -12,7 +12,9 @@ const PopUp = (props) => {
         <div className='popUp'>  
           <div class="pHeader">
             <div class="pChild">{props.title}     
-                For reservation and enquires - Send us <NavLink to={{ pathname: "/email", state: { modal: true }, }} className="Nav_link">Message/E-Mail</NavLink>.</div>
+                For reservation and enquires - Send us 
+                <NavLink to={{ pathname: "/email", state: { modal: true }, }} className="Nav_link">Message/E-Mail</NavLink>
+              </div>
             <div class="pCloseMe"><a href="#" onClick={props.handleClose}>x</a></div>
           </div>        
           <div class="popUp-content">
