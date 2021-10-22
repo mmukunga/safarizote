9d38def9b5065b633e2966eaae8bb16f26d3f1a3
Simon Mukunga <mmukunga@gmail.com>
2021-10-22 22:40:03 +0200

    first commit

--- webapp/src/pages/Safaris.jsx
+++ webapp/src/pages/Safaris.jsx
@@ -36,6 +36,16 @@ const Safaris = () => {
           dateCreated:  moment.now()
     });
 
+    const addToCart = (el) => {
+      setCart([...cart, el]);
+    };
+  
+    const removeFromCart = (el) => {
+      let hardCopy = [...cart];
+      hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
+      setCart(hardCopy);
+    };
+
     const videoUrl = 'https://www.youtube.com/watch?v=3qW5z4xeiac';
 
     const handleClick = (event) => {
@@ -131,17 +141,6 @@ const Safaris = () => {
   const SafariTours = props => {
     const Accordion = ({children, data, title, summary, video}) => {
       const [open, setOpen] = useState(false);
-      const [cart, setCart] = useState([]);
-
-      const addToCart = (el) => {
-        setCart([...cart, el]);
-      };
-    
-      const removeFromCart = (el) => {
-        let hardCopy = [...cart];
-        hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
-        setCart(hardCopy);
-      };
 
       const handleClose = () => {
         setOpen(false)
@@ -156,7 +155,7 @@ const Safaris = () => {
           </div>
           <div dangerouslySetInnerHTML={{__html: summary}} /> 
           <span className='sub' onClick={() => setOpen(true)}>Details and Offers</span>
-          <PopUp data={data} open={open} title={parse(title)} addToCart={addToCart} removeFromCart={removeFromCart} handleClose={handleClose}>{children}</PopUp>
+          <PopUp data={data} open={open} title={parse(title)} addToCart={props.addToCart} removeFromCart={props.removeFromCart} handleClose={handleClose}>{children}</PopUp>
           <div className='clearfix'></div>
         </div>
       )
@@ -216,7 +215,7 @@ const Safaris = () => {
         </div> 
              
         {currentItems && currentItems.length > 0 
-           ? <SafariTours data={currentItems} videos={videos}/> 
+           ? <SafariTours data={currentItems} addToCart={addToCart} removeFromCart={removeFromCart} videos={videos}/> 
            : <p>No Data Found!!</p>}                   
     </Card>
   );
