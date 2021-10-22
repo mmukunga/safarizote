e9e8357d9d16c4c16dd2942cd341909ae2354900
Simon Mukunga <mmukunga@gmail.com>
2021-10-22 15:36:44 +0200

    first commit

--- src/main/java/com/example/safarizote/SafarizoteApplication.java
+++ src/main/java/com/example/safarizote/SafarizoteApplication.java
@@ -2,6 +2,8 @@ package com.example.safarizote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
@@ -10,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.example.safarizote.model.Safari;
+*/
 
 @SpringBootApplication
 public class SafarizoteApplication {
@@ -17,17 +20,17 @@ public class SafarizoteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SafarizoteApplication.class, args);
 	}
-
+/*
     @Bean
     @Scope("application")
     public Map<Integer, Safari> catalog() {
         Map<Integer, Safari> items = new HashMap<>();
-        items.put(0, Safari.builder().id(0L).title("Apple Mac Book Pro").details("<div class='SafariDetails'>").summary("<p>Lake Nakuru</p>").price(2499.99).dateCreated(Instant.now()).build());
-        items.put(1, Safari.builder().id(1L).title("32GB San Disk").details("<div class='SafariDetails'>").summary("<p>Lake Nakuru</p>").price(15.99).dateCreated(Instant.now()).build());
-        items.put(2, Safari.builder().id(2L).title("OLED 64in TV").details("<div class='SafariDetails'>").summary("<p>Lake Nakuru</p>").price(1800.99).dateCreated(Instant.now()).build());
-        items.put(3, Safari.builder().id(3L).title("Wireless Mouse").details("<div class='SafariDetails'>").summary("<p>Lake Nakuru</p>").price(10.59).dateCreated(Instant.now()).build());
-        items.put(4, Safari.builder().id(4L).title("Virtual Reality HeadSet").details("<div class='SafariDetails'>").summary("<p>Lake Nakuru</p>").price(200.59).dateCreated(Instant.now()).build());
-        items.put(5, Safari.builder().id(5L).title("Sat Nav").details("<div class='SafariDetails'>").summary("<p>Lake Nakuru</p>").price(159.99).dateCreated(Instant.now()).build());
+        items.put(0, Safari.builder().id(0L).title("3 days Masai Mara").details("Masai Mara").summary("Mara by Car").price(2499.99).dateCreated(Instant.now()).build());
+        items.put(1, Safari.builder().id(1L).title("One day Nairobi City Tour").details("Nairobi City").summary("Ngong Karen Etc..").price(15.99).dateCreated(Instant.now()).build());
+        items.put(2, Safari.builder().id(2L).title("Two days Amboseli").details("Amboseli and Tsavo").summary("Amboseli").price(1800.99).dateCreated(Instant.now()).build());
+        items.put(3, Safari.builder().id(3L).title("Mount Kenya 5 Days").details("Mount Kenya").summary("Mount Kenya").price(10.59).dateCreated(Instant.now()).build());
+        items.put(4, Safari.builder().id(4L).title("Kilimanjaro 6 days").details("Kilimanjaro").summary("Kilimanjaro").price(200.59).dateCreated(Instant.now()).build());
+        items.put(5, Safari.builder().id(5L).title("Lake Nakuru").details("Lake Nakuru").summary("Lake Nakuru").price(159.99).dateCreated(Instant.now()).build());
         return items;
-    }
+    } */
 }
--- src/main/java/com/example/safarizote/utils/CartServiceImpl.java
+++ src/main/java/com/example/safarizote/utils/CartServiceImpl.java
@@ -2,15 +2,18 @@ package com.example.safarizote.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.example.safarizote.model.Safari;
 import com.example.safarizote.repository.CartRepository;
 
 @Component
-public class CartServiceImpl implements CartService {
+public class CartServiceImpl implements CartService, CommandLineRunner {
 
     @Autowired
     private CartRepository repository;
@@ -29,6 +32,19 @@ public class CartServiceImpl implements CartService {
 
     @Value("${safaris.deliveryCharge.threshold}")
     private double deliveryChargeThreshold;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        catalog = new HashMap<>();
+        catalog.put(0, Safari.builder().id(0L).title("3 days Masai Mara").details("Masai Mara").summary("Mara by Car").price(2499.99).dateCreated(Instant.now()).build());
+        catalog.put(1, Safari.builder().id(1L).title("One day Nairobi City Tour").details("Nairobi City").summary("Ngong Karen Etc..").price(15.99).dateCreated(Instant.now()).build());
+        catalog.put(2, Safari.builder().id(2L).title("Two days Amboseli").details("Amboseli and Tsavo").summary("Amboseli").price(1800.99).dateCreated(Instant.now()).build());
+        catalog.put(3, Safari.builder().id(3L).title("Mount Kenya 5 Days").details("Mount Kenya").summary("Mount Kenya").price(10.59).dateCreated(Instant.now()).build());
+        catalog.put(4, Safari.builder().id(4L).title("Kilimanjaro 6 days").details("Kilimanjaro").summary("Kilimanjaro").price(200.59).dateCreated(Instant.now()).build());
+        catalog.put(5, Safari.builder().id(5L).title("Lake Nakuru").details("Lake Nakuru").summary("Lake Nakuru").price(159.99).dateCreated(Instant.now()).build());
+    }    
+
 
     public String getContactEmail() {
         return contactEmail;
