1007763d6198a9106ac353a0c69dc403a0ddfed2
Simon Mukunga <mmukunga@gmail.com>
2021-10-24 23:49:24 +0200

    first commit

--- webapp/src/pages/Cart.jsx
+++ webapp/src/pages/Cart.jsx
@@ -14,23 +14,12 @@ const Cart = (props) => {
 
   console.log('!!SHOW TRUE!! ' + props.show);
 
-  const modalCart = props.show && ( <div className='overlay'>
-  <div className='popUp'>  
-    <div class="pHeader">
-      <div class="pChild"><span className="SafariTitle">Cart</span>     
-          For reservation and enquires - Send us a 
-        </div>
-      <div class="pCloseMe"><a href="#" onClick={props.handleShow}>X</a></div>
-    </div>        
-    <div class="popUp-content">
-         content HERE
-    </div>
-  </div>
-</div> );
-
-  console.log('!!modalCart!! ' + modalCart);
-
-  return ReactDOM.createPortal('modal', document.getElementById("cart_portal"));
+  return ReactDOM.createPortal(
+    <div>
+        Modal content goes here
+    </div>,
+    document.getElementById('portal-root')
+)
 }
 
 export default Cart;
