22f6e30671e5fcf1328bd8e22d40cd12c8246817
Simon Mukunga <mmukunga@gmail.com>
2021-10-24 21:30:33 +0200

    first commit

--- webapp/src/pages/Cart.jsx
+++ webapp/src/pages/Cart.jsx
@@ -5,12 +5,12 @@ import { NavLink } from "react-router-dom";
 const Cart = (props) => {
   
   console.log(props);
-
+/*
   if (!props.show) { 
     console.log('SHOW!! ' + props.show);
     return null;
   }
-  
+*/
 
   console.log('!!SHOW TRUE!! ' + props.show);
 
