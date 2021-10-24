571f60d1e429466de3f12d48b11d7870873b4149
Simon Mukunga <mmukunga@gmail.com>
2021-10-24 14:53:35 +0200

    first commit

--- webapp/src/pages/Cart.jsx
+++ webapp/src/pages/Cart.jsx
@@ -9,22 +9,7 @@ const Cart = (props) => {
     return null;
   }
    
-  const modalCart = props.show && (
-    <div className='overlay'>
-       <div className='popUp'>  
-         <div class="pHeader">
-           <div class="pChild"><span className="SafariTitle">Cart</span>     
-               For reservation and enquires - Send us a 
-               <NavLink to={{ pathname: "/email", state: { modal: true }, }} className="Nav_link urlStyle">Message or E-Mail</NavLink>
-             </div>
-           <div class="pCloseMe"><a href="#" onClick={props.handleShow}>X</a></div>
-         </div>        
-         <div class="popUp-content">
-         content HERE
-         </div>
-       </div>
-     </div>
-   );
+  const modalCart = props.show && ( <div>CART</div> );
 
   return ReactDOM.createPortal(modalCart, document.getElementById("portal"));
 }
