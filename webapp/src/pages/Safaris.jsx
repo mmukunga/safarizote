016836121e3515841a1082625115afff23109b2b
Simon Mukunga <mmukunga@gmail.com>
2021-10-24 23:57:48 +0200

    first commit

--- webapp/src/pages/Safaris.jsx
+++ webapp/src/pages/Safaris.jsx
@@ -195,7 +195,7 @@ const Safaris = () => {
               <span className='sub' onClick={() => setOpen(true)}>Details and Offers</span>
               <label className='sub'><input type="checkbox" name="check" checked={checked} onClick={handleCart}/>Add To Cart</label>  
               <span className='sub' onClick={() => setShow(true)}>Show Cart</span>       
-              {cart.length < 1? ' Cart is Empty' : <input type="button" onClick={() => handleSubmit()}>Send Booking</input>}
+              {cart.length < 1? ' Cart is Empty' : <label className='sub'><input type="button" onClick={() => handleSubmit()}/>Send Booking</label>}
               <PopUp data={data} open={open} title={parse(title)} handleClose={handleClose}>{children}</PopUp>
           </div>
           <div className='clearfix'></div>
