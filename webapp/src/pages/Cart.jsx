e0b7ec6d7434644a9a5f96a4ebb51db9e5375b91
Simon Mukunga <mmukunga@gmail.com>
2021-10-24 23:28:46 +0200

    first commit

--- webapp/src/pages/Cart.jsx
+++ webapp/src/pages/Cart.jsx
@@ -24,8 +24,6 @@ const Cart = (props) => {
     </div>        
     <div class="popUp-content">
          content HERE
-         {props.cart.length > 0 ? props.cart.map((item) => <div key={item.id}>{item.id} {item.title}</div>): 'No items in Cart'
-          }
     </div>
   </div>
 </div> );
