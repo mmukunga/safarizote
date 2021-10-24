7941342d4420a789a7145799af6790fb7f653071
Simon Mukunga <mmukunga@gmail.com>
2021-10-24 15:28:01 +0200

    first commit

--- webapp/src/pages/Cart.jsx
+++ webapp/src/pages/Cart.jsx
@@ -5,13 +5,18 @@ import { NavLink } from "react-router-dom";
 const Cart = (props) => {
   
   console.log(props);
-  
+
   if (!props.show) { 
     console.log('SHOW!! ' + props.show);
     return null;
   }
-   
+  
+
+  console.log('!!SHOW TRUE!! ' + props.show);
+
   const modalCart = props.show && ( <div>CART</div> );
+
+  console.log('!!modalCart!! ' + modalCart);
 
   return ReactDOM.createPortal(modalCart, document.getElementById("portal"));
 }
