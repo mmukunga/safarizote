66442fc96ec7f8fe8d9e079b719cd812c7a514cf
Simon Mukunga <mmukunga@gmail.com>
2021-11-01 09:54:22 +0100

    first commit

--- src/main/java/com/example/safarizote/repository/SafariLoader.java
+++ src/main/java/com/example/safarizote/repository/SafariLoader.java
@@ -93,16 +93,21 @@ public class SafariLoader implements CommandLineRunner {
 
     public List<Booking> createBookings() {
         List<Booking> bookings = new ArrayList<>();
+        List<Safari> safaris = repository.findAll();
+        Safari[] myArray = new Safari[safaris.size()];
+        safaris.toArray(myArray);
 
-        bookings.add(Booking.builder().name("Jack Maji Moto Smith").email("m@gmail.com").phone("415 22 386").address("21 Jump street").dateCreated(Instant.now()).build());
-        bookings.add(Booking.builder().name("Adam Moto Wake").email("maji@gmail.com").phone("915 22 111").address("Grefsen Platåen").dateCreated(Instant.now()).build());
-        bookings.add(Booking.builder().name("Johnson Katana Ndovu").email("moto@hotmail.com").phone("222 22 222").address("Maridalsveien").dateCreated(Instant.now()).build());
-        bookings.add(Booking.builder().name("Peter Ngara Mwendwa").email("kazi@online.no").phone("911 22 911").address("Number 10").dateCreated(Instant.now()).build());
-        bookings.add(Booking.builder().name("Masinde Murilo David").email("sverige@kora.se").phone("+44 510 22 777").address("Downings Street").dateCreated(Instant.now()).build());
+        int id = (int) ((Math.random() * (myArray.length - 0)) + 0);
+
+        bookings.add(Booking.builder().safariId(Long.valueOf(id)).name("Jack Maji Moto Smith").email("m@gmail.com").phone("415 22 386").address("21 Jump street").dateCreated(Instant.now()).build());
+        bookings.add(Booking.builder().safariId(Long.valueOf(id)).name("Adam Moto Wake").email("maji@gmail.com").phone("915 22 111").address("Grefsen Platåen").dateCreated(Instant.now()).build());
+        bookings.add(Booking.builder().safariId(Long.valueOf(id)).name("Johnson Katana Ndovu").email("moto@hotmail.com").phone("222 22 222").address("Maridalsveien").dateCreated(Instant.now()).build());
+        bookings.add(Booking.builder().safariId(Long.valueOf(id)).name("Peter Ngara Mwendwa").email("kazi@online.no").phone("911 22 911").address("Number 10").dateCreated(Instant.now()).build());
+        bookings.add(Booking.builder().safariId(Long.valueOf(id)).name("Masinde Murilo David").email("sverige@kora.se").phone("+44 510 22 777").address("Downings Street").dateCreated(Instant.now()).build());
         
         Set<Booking> hSet = new HashSet<Booking>(bookings);
 
-        List<Safari> safaris = repository.findAll(); 
+        safaris = repository.findAll(); 
         for (int i = 0; i < safaris.size(); i++) {
             Safari safari = safaris.get(i);
             safari.setBookings(hSet);
