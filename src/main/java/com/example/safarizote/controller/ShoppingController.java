7afeacb2d045aec500f6317cf8fac182abe8a5f4
Simon Mukunga <mmukunga@gmail.com>
2021-11-01 16:29:59 +0100

    first commit

--- src/main/java/com/example/safarizote/controller/ShoppingController.java
+++ src/main/java/com/example/safarizote/controller/ShoppingController.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -37,4 +39,11 @@ public class ShoppingController {
 			return ResponseEntity.ok().build();
 		}
 	}
+
+ @DeleteMapping
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+    	repository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+
 }
