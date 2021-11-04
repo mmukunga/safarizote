c568387b4f05e988b5c09ed7029756d42c75128b
Simon Mukunga <mmukunga@gmail.com>
2021-11-04 22:18:26 +0100

    first commit

--- src/main/java/com/example/safarizote/model/Safari.java
+++ src/main/java/com/example/safarizote/model/Safari.java
@@ -2,14 +2,14 @@ package com.example.safarizote.model;
 
 import java.time.Instant;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Column;
-import javax.persistence.ManyToOne;
-import javax.persistence.JoinColumn;
-import javax.persistence.PrePersist;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
