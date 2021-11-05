ac5687cd239824a9afc4377c28e3d6b8f398104a
Simon Mukunga <mmukunga@gmail.com>
2021-11-05 17:27:29 +0100

    first commit

--- webapp/src/pages/Bookings.jsx
+++ webapp/src/pages/Bookings.jsx
@@ -1,48 +1,8 @@
 import React from "react";
+import { useCustomContext } from './CustomContext';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'INIT_BOOKING': {
-      console.log(action);
-      const nextId = state.nextId + 1;
-      if (action.payload.type === "checkbox") {
-        return {
-          ...state,
-        [action.payload.name]: action.payload.checked
-        }
-      }
-      const {name, value} = action.payload;
-      console.log(action.payload);
-      return {
-        ...state,
-        [name]: value,
-      };
-    }
-    case 'ADD_TOUR': {
-      console.log(action);
-      const nextId = state.nextId + 1;
-      const {name, value} = action.payload;
-      console.log(action.payload);
-      return {
-        nextId,
-        tours: [...state.tours, { nextId: nextId, [name]: value }],
-      };
-    }
-    case 'REMOVE_TOUR': {
-      const nextId = state.nextId;
-      const result = state.tours.filter((tour) => tour.nextId !== action.payload);
-      return {
-        nextId,
-        tours: [...result],
-      };
-    }
-    default:
-      return state;
-  }
-};
-
-const Bookings = ({ onChange, initialState }) => {
-  const [state, dispatch] = React.useReducer(reducer, initialState);  
+const Bookings = (props) => {
+  const [state, dispatch] = useCustomContext(); 
   const Tour = ({ nextId, title, onRemove }) => {
     console.log('1..tour..');
     console.log(title);
@@ -60,7 +20,7 @@ const Bookings = ({ onChange, initialState }) => {
   console.log(state.tours);
 
   const handleClick = el => {
-    onChange(el); 
+    props.onChange(el); 
   }
 
   const handleChange = el => {
--- webapp/src/pages/Safaris.jsx
+++ webapp/src/pages/Safaris.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CustomContext from './CustomContext';
 import Bookings from './Bookings';
 
 
@@ -14,21 +15,64 @@ const initialState = {
     tours: [{nextId: 0, title:'Masaai Mara' }],
   };
 
+  const bookingsReducer = (state, action) => {
+    switch (action.type) {
+      case 'INIT_BOOKING': {
+        console.log(action);
+        if (action.payload.type === "checkbox") {
+          return {
+            ...state,
+          [action.payload.name]: action.payload.checked
+          }
+        }
+        const {name, value} = action.payload;
+        console.log(action.payload);
+        return {
+          ...state,
+          [name]: value,
+        };
+      }
+      case 'ADD_TOUR': {
+        console.log(action);
+        const nextId = state.nextId + 1;
+        const {name, value} = action.payload;
+        console.log(action.payload);
+        return {
+          nextId,
+          tours: [...state.tours, { nextId: nextId, [name]: value }],
+        };
+      }
+      case 'REMOVE_TOUR': {
+        const nextId = state.nextId;
+        const result = state.tours.filter((tour) => tour.nextId !== action.payload);
+        return {
+          nextId,
+          tours: [...result],
+        };
+      }
+      default:
+        return state;
+    }
+  };
+
 const Safaris = () => {
     const [name, setName] = React.useState('');
+    const [state, dispatch ] = React.useReducer(bookingsReducer, initialState);
+    const providerState = { state, dispatch };
+
     const handleChange = (el) => {
         const {name, value} = el.target;
         console.log(name + ': ' + value);
         setName(value);
     }
 
-// We pass a callback to Child
+    // We pass a callback to Child
     return ( 
-    <div>
+    <CustomContext.Provider value={providerState} >
         <h2>Count {name}</h2>
         (Count should be updated from child)
         <Bookings initialState={initialState} onChange={handleChange}/>
-    </div>
+    </CustomContext.Provider>
     );
 }
 
