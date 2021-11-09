7e8a76a60c028b43ecddb9ff9880618e6ee2dda5
Simon Mukunga <mmukunga@gmail.com>
2021-11-09 07:39:35 +0100

    first commit

--- webapp/src/pages/Modal.jsx
+++ webapp/src/pages/Modal.jsx
@@ -24,104 +24,103 @@ const Modal = (props) => {
   if (!props.isOpen) {
       return null;
   }
-}
 
-const modal = ({handleChange, handleSubmit, toggle, options}) => {
- return( <div className="PopUp-Container">
-        <div className="PopUp-Content">
-          <span className="close-icon" onClick={toggle}>x</span>
-          {props.content} 
+  const modal = ({handleChange, handleSubmit, toggle, options}) => {
+  return( <div className="PopUp-Container">
+          <div className="PopUp-Content">
+            <span className="close-icon" onClick={toggle}>x</span>
+            {props.content} 
 
-          <Form schema={schema}
-                  onChange={handleChange}
-                  onSubmit={handleSubmit}
-                  onError={console.log("errors")}/>
+            <Form schema={schema}
+                    onChange={handleChange}
+                    onSubmit={handleSubmit}
+                    onError={console.log("errors")}/>
 
-          <form onSubmit={handleSubmit}> 
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="name">Name</label>
+            <form onSubmit={handleSubmit}> 
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="name">Name</label>
+                </div>
+                <div className="col-75">
+                  <input id='name' name="name" onChange={handleChange}/>
+                </div>
               </div>
-              <div className="col-75">
-                <input id='name' name="name" onChange={handleChange}/>
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="email">Email</label>
+                </div>
+                <div className="col-75">
+                <input id='email' name="email" onChange={handleChange}/>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="email">Email</label>
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="date">Arrival Date</label>
+                </div>
+                <div className="col-75">
+                <input id='date' name="date" onChange={handleChange}/>
+                </div>
               </div>
-              <div className="col-75">
-              <input id='email' name="email" onChange={handleChange}/>
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="address">Address</label>
+                </div>
+                <div className="col-75">
+                <input id='address' name="address" onChange={handleChange}/>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="date">Arrival Date</label>
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="adults">Adults</label>
+                </div>
+                <div className="col-75">
+                  <input id='adults' name="adults" onChange={handleChange}/>
+                </div>
               </div>
-              <div className="col-75">
-              <input id='date' name="date" onChange={handleChange}/>
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="children">Children</label>
+                </div>
+                <div className="col-75">
+                  <input id='children' name="children" onChange={handleChange}/>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="address">Address</label>
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="isGoing">Is Going</label>
+                </div>
+                <div className="col-75">
+                <input type="checkbox" id='isGoing' name="isGoing" onChange={handleChange}/>
+                </div>
+              </div>     
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="safariId">Safari ID</label>
+                </div>
+                <div className="col-75">
+                <select id="safariId" name="safariId" onChange={handleChange}>
+                    {options.map((option) => (
+                      <option value={option.id}>{option.title}</option>
+                    ))}
+                  </select>
+                </div>
               </div>
-              <div className="col-75">
-              <input id='address' name="address" onChange={handleChange}/>
+              <div className="row">
+                <div className="col-25">
+                  <label forHtml="message">Message</label>
+                </div>
+                <div className="col-75">
+                  <textarea id="message" name="message" onChange={handleChange} style={{height:"100px"}}></textarea>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="adults">Adults</label>
+              <div className="row">
+                <input type="submit" value="Submit"/>
               </div>
-              <div className="col-75">
-                <input id='adults' name="adults" onChange={handleChange}/>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="children">Children</label>
-              </div>
-              <div className="col-75">
-                <input id='children' name="children" onChange={handleChange}/>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="isGoing">Is Going</label>
-              </div>
-              <div className="col-75">
-              <input type="checkbox" id='isGoing' name="isGoing" onChange={handleChange}/>
-              </div>
-            </div>     
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="safariId">Safari ID</label>
-              </div>
-              <div className="col-75">
-              <select id="safariId" name="safariId" onChange={handleChange}>
-                  {options.map((option) => (
-                    <option value={option.id}>{option.title}</option>
-                  ))}
-                </select>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label forHtml="message">Message</label>
-              </div>
-              <div className="col-75">
-                <textarea id="message" name="message" onChange={handleChange} style={{height:"100px"}}></textarea>
-              </div>
-            </div>
-            <div className="row">
-              <input type="submit" value="Submit"/>
-            </div>
-          </form>
-        </div>
-      </div>);
-}
+            </form>
+          </div>
+        </div>);
+  }
 
   return ReactDOM.createPortal(modal(props), document.body);
-
+}
 export default Modal;
