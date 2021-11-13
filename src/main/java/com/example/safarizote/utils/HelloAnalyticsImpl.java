36198ae136072772d1c6e7f1a9378f4ad9b81c93
Simon Mukunga <mmukunga@gmail.com>
2021-11-13 17:47:45 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
@@ -12,23 +12,19 @@ import com.google.api.services.analytics.model.GaData;
 import com.google.api.services.analytics.model.Profiles;
 import com.google.api.services.analytics.model.Webproperties;
 
-import org.apache.tomcat.util.descriptor.web.ResourceBase;
-
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 @Service("gaService")
 public class HelloAnalyticsImpl implements IHelloAnalytics { 
-  private static final String APPLICATION_NAME = "GoogleAnalytics";
+  private static final String APPLICATION_NAME = "gaSafarizote";
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  private static final String KEY_FILE_LOCATION = "gaCredentials.json";
+  private static final String KEY_FILE_LOCATION = "gaSafarizote-2e51f4174cf8.json";
   
     public GaData getGAData() throws Exception {
         System.out.println("1.gaService.getGAData()");
@@ -43,58 +39,40 @@ public class HelloAnalyticsImpl implements IHelloAnalytics {
         return results;
     }
 
-    /**
-   * Initializes an Analytics service object.
-   *
-   * @return An authorized Analytics service object.
-   * @throws IOException
-   * @throws GeneralSecurityException
-   */
   private static Analytics initializeAnalytic() throws GeneralSecurityException, IOException {
-
     HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    // Getting ClassLoader obj
-    //ClassLoader classLoader = getClass().getClassLoader();
+
     Resource resource = new ClassPathResource(KEY_FILE_LOCATION);
     GoogleCredential credential = GoogleCredential
             .fromStream(resource.getInputStream())
             .createScoped(AnalyticsScopes.all());
 
-    // Construct the Analytics service object.
     return new Analytics.Builder(httpTransport, JSON_FACTORY, credential)
         .setApplicationName(APPLICATION_NAME).build();
   }
 
 
   private static String getFirstProfileId(Analytics analytics) throws IOException {
-    // Get the first view (profile) ID for the authorized user.
     String profileId = null;
-
-    // Query for the list of all accounts associated with the service account.
     Accounts accounts = analytics.management().accounts().list().execute();
 
     if (accounts.getItems().isEmpty()) {
       System.err.println("No accounts found");
     } else {
       String firstAccountId = accounts.getItems().get(0).getId();
-
-      // Query for the list of properties associated with the first account.
       Webproperties properties = analytics.management().webproperties()
           .list(firstAccountId).execute();
-
       if (properties.getItems().isEmpty()) {
         System.err.println("No Webproperties found");
       } else {
         String firstWebpropertyId = properties.getItems().get(0).getId();
 
-        // Query for the list views (profiles) associated with the property.
         Profiles profiles = analytics.management().profiles()
             .list(firstAccountId, firstWebpropertyId).execute();
 
         if (profiles.getItems().isEmpty()) {
           System.err.println("No views (profiles) found");
         } else {
-          // Return the first (view) profile associated with the property.
           profileId = profiles.getItems().get(0).getId();
         }
       }
@@ -103,16 +81,12 @@ public class HelloAnalyticsImpl implements IHelloAnalytics {
   }
 
   private static GaData getResults(Analytics analytics, String profileId) throws IOException {
-    // Query the Core Reporting API for the number of sessions
-    // in the past seven days.
     return analytics.data().ga()
         .get("ga:" + profileId, "7daysAgo", "today", "ga:sessions")
         .execute();
   }
 
   private static void printResults(GaData results) {
-    // Parse the response from the Core Reporting API for
-    // the profile name and number of sessions.
     if (results != null && !results.getRows().isEmpty()) {
       System.out.println("View (Profile) Name: "
         + results.getProfileInfo().getProfileName());
