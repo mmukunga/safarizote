abbd4d33207fc53861d2b6645b1e022679d4adf7
Simon Mukunga <mmukunga@gmail.com>
2021-11-15 15:48:55 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
@@ -1,18 +1,10 @@
 package com.example.safarizote.utils;
 
-//import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-//import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.analytics.Analytics;
 import com.google.api.services.analytics.AnalyticsScopes;
 import com.google.api.services.analytics.model.Accounts;
@@ -21,37 +13,19 @@ import com.google.api.services.analytics.model.Profiles;
 import com.google.api.services.analytics.model.Webproperties;
 
 import org.springframework.core.io.ClassPathResource;
-
-//import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 
 @Service("gaService")
 public class HelloAnalyticsImpl implements IHelloAnalytics { 
-  //private static final String APPLICATION_NAME = "GaMajiMoto";
-  //private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  private static final String KEY_FILE_LOCATION = "gcmajimoto-958d87dbada8.json";
-  //
-  //
-  // Path to client_secrets.json file downloaded from the Developer's Console.
-  // The path is relative to HelloAnalytics.java.
-  //private static final String CLIENT_SECRET_JSON_RESOURCE = "gcmajimoto-958d87dbada8.json";
-
-  // The directory where the user's credentials will be stored.
-  private static final File DATA_STORE_DIR =
-      new File(HelloAnalyticsImpl.class.getResource("/").getPath(), "credentials");
-
   private static final String APPLICATION_NAME = "GaMajiMoto";
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  //private static NetHttpTransport httpTransport;
-  private static FileDataStoreFactory dataStoreFactory;
-
+  private static final String KEY_FILE_LOCATION = "gcmajimoto-958d87dbada8.json";
+  
     public GaData getGAData() throws Exception {
         Analytics analytics = initializeAnalytic();
         String profile = getFirstProfileId(analytics);
@@ -64,31 +38,10 @@ public class HelloAnalyticsImpl implements IHelloAnalytics {
     HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
     Resource resource = new ClassPathResource(KEY_FILE_LOCATION);
-    /*GoogleCredential credential = GoogleCredential
+    GoogleCredential credential = GoogleCredential
                 .fromStream(resource.getInputStream())
-                .createScoped(AnalyticsScopes.all());*/
+                .createScoped(AnalyticsScopes.all());
     
-    httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
-
-    // Load client secrets.
-    /*GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-        new InputStreamReader(HelloAnalyticsImpl.class
-            .getResourceAsStream(KEY_FILE_LOCATION)));*/
-    
-            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-            new InputStreamReader(resource.getInputStream()));
-                        
-    // Set up authorization code flow for all auth scopes.
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
-        .Builder(httpTransport, JSON_FACTORY, clientSecrets,
-        AnalyticsScopes.all()).setDataStoreFactory(dataStoreFactory)
-        .build();
-
-    // Authorize.
-    Credential credential = new AuthorizationCodeInstalledApp(flow,
-        new LocalServerReceiver()).authorize("user");
-                                
     return new Analytics.Builder(httpTransport, JSON_FACTORY, credential)
         .setApplicationName(APPLICATION_NAME).build();
   }
