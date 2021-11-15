d28906c43b9387deb65b4c59d9529f968579a58d
Simon Mukunga <mmukunga@gmail.com>
2021-11-15 12:46:01 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
@@ -1,53 +1,89 @@
 package com.example.safarizote.utils;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+//import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
+//import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.analytics.Analytics;
 import com.google.api.services.analytics.AnalyticsScopes;
 import com.google.api.services.analytics.model.Accounts;
 import com.google.api.services.analytics.model.GaData;
 import com.google.api.services.analytics.model.Profiles;
 import com.google.api.services.analytics.model.Webproperties;
-import com.google.api.services.analytics.model.Webproperty;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.Resource;
 
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 
 @Service("gaService")
 public class HelloAnalyticsImpl implements IHelloAnalytics { 
+  //private static final String APPLICATION_NAME = "GaMajiMoto";
+  //private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+  //private static final String KEY_FILE_LOCATION = "gcmajimoto-958d87dbada8.json";
+  //
+  //
+  // Path to client_secrets.json file downloaded from the Developer's Console.
+  // The path is relative to HelloAnalytics.java.
+  private static final String CLIENT_SECRET_JSON_RESOURCE = "gcmajimoto-958d87dbada8.json";
+
+  // The directory where the user's credentials will be stored.
+  private static final File DATA_STORE_DIR = new File(
+      System.getProperty("user.home"), ".store/hello_analytics");
+
   private static final String APPLICATION_NAME = "GaMajiMoto";
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  private static final String KEY_FILE_LOCATION = "gcmajimoto-958d87dbada8.json";
-  
+  //private static NetHttpTransport httpTransport;
+  private static FileDataStoreFactory dataStoreFactory;
+
     public GaData getGAData() throws Exception {
-        System.out.println("1.gaService.getGAData()");
         Analytics analytics = initializeAnalytic();
-        System.out.println("2.gaService.getGAData()");
         String profile = getFirstProfileId(analytics);
-        System.out.println("3.gaService.getGAData()");
         GaData results = getResults(analytics, profile);
-        System.out.println("4.gaService.getGAData()");
         printResults(getResults(analytics, profile));
-        System.out.println("5.gaService.getGAData()");
         return results;
     }
 
   private static Analytics initializeAnalytic() throws GeneralSecurityException, IOException {
     HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
-    Resource resource = new ClassPathResource(KEY_FILE_LOCATION);
-    GoogleCredential credential = GoogleCredential
-            .fromStream(resource.getInputStream())
-            .createScoped(AnalyticsScopes.all());
+    //Resource resource = new ClassPathResource(KEY_FILE_LOCATION);
+    /*GoogleCredential credential = GoogleCredential
+                .fromStream(resource.getInputStream())
+                .createScoped(AnalyticsScopes.all());*/
+    
+    httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
 
+    // Load client secrets.
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+        new InputStreamReader(HelloAnalyticsImpl.class
+            .getResourceAsStream(CLIENT_SECRET_JSON_RESOURCE)));
+
+    // Set up authorization code flow for all auth scopes.
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
+        .Builder(httpTransport, JSON_FACTORY, clientSecrets,
+        AnalyticsScopes.all()).setDataStoreFactory(dataStoreFactory)
+        .build();
+
+    // Authorize.
+    Credential credential = new AuthorizationCodeInstalledApp(flow,
+        new LocalServerReceiver()).authorize("user");
+                                
     return new Analytics.Builder(httpTransport, JSON_FACTORY, credential)
         .setApplicationName(APPLICATION_NAME).build();
   }
@@ -60,45 +96,9 @@ public class HelloAnalyticsImpl implements IHelloAnalytics {
       System.err.println("No accounts found");
     } else {
       String firstAccountId = accounts.getItems().get(0).getId();
-      System.out.println("firstAccountId:= " + firstAccountId);
-      Webproperties properties = analytics.management().webproperties()
-          .list(firstAccountId).execute();
-      System.out.println("properties:= " + properties);
-      
-      for (Webproperty property : properties.getItems()) {
-        System.out.println("Account ID: " + property.getAccountId());
-        System.out.println("Property ID: " + property.getId());
-        System.out.println("Property Name: " + property.getName());
-        System.out.println("Property Profile Count: " + property.getProfileCount());
-        System.out.println("Property Industry Vertical: "
-            + property.getIndustryVertical());
-        System.out.println("Property Internal Id: "
-            + property.getInternalWebPropertyId());
-        System.out.println("Property Level: " + property.getLevel());
-        if (property.getWebsiteUrl() != null) {
-          System.out.println("Property URL: " + property.getWebsiteUrl());
-        }
-        System.out.println("Property Created: " + property.getCreated());
-        System.out.println("Property Updated: " + property.getUpdated());
-      }
-
-      Webproperties properties2 = analytics.management().webproperties().list("~all").execute();
-      for (Webproperty property : properties2.getItems()) {
-        System.out.println("Account ID: " + property.getAccountId());
-        System.out.println("Property ID: " + property.getId());
-        System.out.println("Property Name: " + property.getName());
-        System.out.println("Property Profile Count: " + property.getProfileCount());
-        System.out.println("Property Industry Vertical: "
-            + property.getIndustryVertical());
-        System.out.println("Property Internal Id: "
-            + property.getInternalWebPropertyId());
-        System.out.println("Property Level: " + property.getLevel());
-        if (property.getWebsiteUrl() != null) {
-          System.out.println("Property URL: " + property.getWebsiteUrl());
-        }
-        System.out.println("Property Created: " + property.getCreated());
-        System.out.println("Property Updated: " + property.getUpdated());
-      }
+      //Webproperties properties0 = analytics.management().webproperties()
+      //    .list(firstAccountId).execute();
+      Webproperties properties = analytics.management().webproperties().list("~all").execute();
 
       if (properties.getItems().isEmpty()) {
         System.err.println("No Webproperties found");
