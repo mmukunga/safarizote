39cb985b05af42388a9823a909f60e4d6def222c
Simon Mukunga <mmukunga@gmail.com>
2021-11-20 06:49:29 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
@@ -67,47 +67,35 @@ public class HelloAnalyticsImpl implements IHelloAnalytics {
 
   private static Analytics initializeAnalytic() throws GeneralSecurityException, IOException {
     HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-
-    Resource resource = new ClassPathResource(KEY_FILE_LOCATION);
-    GoogleCredential credential = GoogleCredential.fromStream(resource.getInputStream())
-        .createScoped(AnalyticsScopes.all());
-
-    System.out.println("1.DATA_STORE_DIR");
     if(DATA_STORE_DIR.exists()){
       System.out.println("DATA_STORE_DIR EXISTS");
     } else {
       DATA_STORE_DIR.mkdirs();
     }
 
-    System.out.println("2.DATA_STORE_DIR:= " + DATA_STORE_DIR);
     httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    System.out.println("3.DATA_STORE_DIR");
     dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
     System.out.println(HelloAnalyticsImpl.class.getClassLoader().getResourceAsStream(KEY_FILE_LOCATION));
-    System.out.println("4.DATA_STORE_DIR dataStoreFactory:= " + dataStoreFactory);
     // Load client secrets.
     JsonFactory JSON_FACTORY =JacksonFactory.getDefaultInstance();
     GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-        new InputStreamReader(HelloAnalyticsImpl.class.getResourceAsStream("/gcmajimoto-958d87dbada8.json")));
+        new InputStreamReader(HelloAnalyticsImpl.class.getClassLoader().getResourceAsStream("gcmajimoto-958d87dbada8.json")));
     
     //System.out.println(clientSecrets.getDetails().getClientSecret());
-    //System.out.println(clientSecrets.getDetails().getClientId());
+    System.out.println(clientSecrets);
 
-    java.util.Collection<java.lang.String> scopes =  AnalyticsReportingScopes.all();             // View your Google Analytics data
-    //        File file = new File(TestCalendar2.class.getResource("/eb2e54acd629.p12").getFile());
-    Collection<String> scopes2 = new ArrayList<String>();
     GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
       httpTransport, 
       JSON_FACTORY,
       "106423389543943811455", 
       "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDb3NyxcPPDBVBe\nRRHvZY6OKPTlaKtH4FtcwFZhrFvOB0IoQ0VkQkKJR7re28vXOY15Jijf9Fma3yZQ\nT8TFjmXT74lWlDq+j6tCaTJVhcdw7b+QJOaSTeFq2R35o28RjdkRlKQq3dP3hj9C\ncMvVDL3uLyrtE5rZCEMOCAA5qiEX3TsZzeGmdEN0VgQykIkSpQolpJwt6suvfQeP\n7BNC97PccY3LQViYFFQljrJVGgoqUrnjl/2onyTdjgvkGRNaKQ3+i3JVkvkvlSOu\n7ftx0CbOOBgTFGBDQydFf33Dy42ovu60WN3je0swyBWzuXfEDPU6gXjt9yiP6ZUE\ns20bC/lFAgMBAAECggEAbSSbY/SL9ayOJHYKPLrABT88ryOCH4i3IfiTCSKsYj8F\nLwKSEH66E/cJQ7d93eZCnjj8woxE7RZIqUaoCmAxmX+IQHQOI2ojnLCnHYBvLNbB\nKVfAJDHILKgCctoYeXDeqJUO1/7NyksY3S0saqNMJ9C/cPgx/vpUe0MqhXW4vuSW\n8yRdb7JIb5cqKdqEegBG4plfPLv6eekba9lsH1GjRgGNIsHpWG3c6ooyvtBhVztl\nVVO6kmjRTdnUuOiC8hCOlxlfauq2793m2O40jyEge646zAJG7Av7g+ujAElK8d5i\nhSyniHIW4DclU3D/QntCmw52enSgf9G2uCBStdZukwKBgQDveFe+uXogjt8YxVfw\nfp6gQPsUsAu9I2Zmfh+q6q368V755Ro3z42H9auwZntO84QhvfqmjGA3OpAAXgrh\nlbOe13H3PtQTxYzj71bkKDD+8rjr/KISvvtwqU7quVrX5Wt+H+PEgLgq1YaOGgps\nyJsEF9dAFf5IIC0C4E5ToOoV5wKBgQDrCgoY1l9I8GnG1yhQFi5bTCulMhPEsgLw\nVRkDv4JOZaJ3OP/PjASG1gcnlZNZDL4OuGYhKiOaJbM8UvsoLr8m5Wk2H9JCLdFB\nkkPy/XQ+A3j7y37XoKfjrvWoQnfEOoEefPGsMYSqSFTQ2bSv66j7zJFaNkRq64z5\n3r9rRal58wKBgQCtIO8nF4dhBInsqbgTSie6qmAcK87A1lgRH3wboIFaHKlSt70F\nvouPUSDobp8VjF7p2iTdxj1FkJRNf7AUOZvb6d0eKJFBHiDnJcFHQHtzxt3E1ygB\nr4uvfuhsMMepsNbPH5UXGwyLA0a9c7w3u6y+/9GCJqRpVsHUA4D9WygB3wKBgGt8\nnN988fh2UbloXfw6GJsR+4fBnf6+9c8Y66amJx5ZJnJlc79jphJPwNfedJMt26D5\nvWL/VWbCRPSaclePAUX1dicVaF9xepgFHuP5+vX9Oo/UIT7VLtJWCewX8ZOgubnK\no+7+elAxYfBdD1/AYvg8NmDOHn3TGRaQQnP5WKb3AoGAU6+dL42MesRRaeSnoI3q\nM+uzH6wdfouwWo1x2nTnhc7X9ae3Xu+ymmI+A1ox8vt6arpsX9GHT13nEZpN/xlX\nioWSycxLTRjhnPKpe8EtJ/KITcTO+Ixh+68iyC6JOEf0ZMZP46Cv+GgZEGS8Z3xG\nBVZtBnW6tcrVniL4wi6H6wg=\n-----END PRIVATE KEY-----\n", 
-      scopes)
+      AnalyticsReportingScopes.all())
       .setDataStoreFactory(dataStoreFactory)
       .setAccessType("offline").build();
 
     System.out.println("5.DATA_STORE_DIR");
-    Credential ct = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-    System.out.println("6.DATA_STORE_DIR:= " + ct);
+    Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    System.out.println("6.DATA_STORE_DIR:= " + credential);
 
     return new Analytics.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
   }
