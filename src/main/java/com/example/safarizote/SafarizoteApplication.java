e26ddc0f3170bda66697546cd875b159545355c8
Simon Mukunga <mmukunga@gmail.com>
2021-11-21 06:23:05 +0100

    first commit

--- src/main/java/com/example/safarizote/SafarizoteApplication.java
+++ src/main/java/com/example/safarizote/SafarizoteApplication.java
@@ -1,12 +1,13 @@
 package com.example.safarizote;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SafarizoteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SafarizoteApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(SafarizoteApplication.class);
+        builder.headless(false).run(args);
 	}
 }
