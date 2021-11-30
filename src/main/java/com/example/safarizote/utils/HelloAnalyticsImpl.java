d410e45c932c2402c6ce47929397c2c94b3afe1f
Simon Mukunga <mmukunga@gmail.com>
2021-11-30 06:32:31 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
@@ -1,131 +1,120 @@
 package com.example.safarizote.utils;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.auth.oauth2.Credential;
-
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 
-import com.google.api.client.http.HttpTransport;
 import com.google.api.services.analytics.Analytics;
+import com.google.api.services.analytics.AnalyticsScopes;
 import com.google.api.services.analytics.model.Accounts;
 import com.google.api.services.analytics.model.GaData;
 import com.google.api.services.analytics.model.Profiles;
 import com.google.api.services.analytics.model.Webproperties;
-import com.google.api.services.analyticsreporting.v4.AnalyticsReportingScopes;
 
-import org.springframework.stereotype.Service;
-
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.io.IOException;
 
 
-@Service("gaService")
-public class HelloAnalyticsImpl implements IHelloAnalytics {
-  private static final String APPLICATION_NAME = "GaMajiMoto";
+/**
+ * A simple example of how to access the Google Analytics API using a service
+ * account.
+ */
+public class HelloAnalyticsImpl {
+
+
+  private static final String APPLICATION_NAME = "Hello Analytics";
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  private static final String KEY_FILE_LOCATION = "gcmajimoto-958d87dbada8.json";
-
-  private static final File DATA_STORE_DIR = new File(new File(System.getProperty("java.io.tmpdir")), "hello_analytics");
-  private static FileDataStoreFactory dataStoreFactory;
-  /** Global instance of the HTTP transport. */
-  private static HttpTransport httpTransport;
+  private static final String KEY_FILE_LOCATION = "<REPLACE_WITH_JSON_FILE>";
 
   public GaData getGAData() throws Exception {
-    Analytics analytics = initializeAnalytic();
-    String profile = getFirstProfileId(analytics);
-    GaData results = getResults(analytics, profile);
-    printResults(getResults(analytics, profile));
-    return results;
-  }
+    try {
+      Analytics analytics = initializeAnalytics();
 
-  private static Analytics initializeAnalytic() throws GeneralSecurityException, IOException {
-   // Authorization.
-   Credential credential = authorize();
-
-    System.out.println("6.credential:= " + credential);
-
-    return new Analytics.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
-  }
-
-  private static Credential authorize() throws GeneralSecurityException, IOException {
-    httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-
-    if(DATA_STORE_DIR.exists()){
-      System.out.println("DATA_STORE_DIR EXISTS");
-    } else {
-      DATA_STORE_DIR.mkdirs();
+      String profile = getFirstProfileId(analytics);
+      System.out.println("First Profile Id: "+ profile);
+      printResults(getResults(analytics, profile));
+      return getResults(analytics, profile);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
-    System.out.println(HelloAnalyticsImpl.class.getClassLoader().getResourceAsStream(KEY_FILE_LOCATION));
-    // Load client secrets.
-    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-        new InputStreamReader(HelloAnalyticsImpl.class.getClassLoader().getResourceAsStream("gcmajimoto-958d87dbada8.json")));
-
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-      httpTransport, 
-      JSON_FACTORY,
-      clientSecrets.get("client_id").toString(), 
-      clientSecrets.get("private_key").toString(),
-      AnalyticsReportingScopes.all())
-      .setDataStoreFactory(dataStoreFactory)
-      .setAccessType("offline").build();
-    
-    System.out.println("5.DATA_STORE_DIR:= " + clientSecrets.get("project_id"));
-    
-    String userId = "mmukunga@gmail.com";
-    Credential credential = flow.loadCredential(userId);
-
-    return credential;
+    return null;
   }
 
+  /**
+   * Initializes an Analytics service object.
+   *
+   * @return An authorized Analytics service object.
+   * @throws IOException
+   * @throws GeneralSecurityException
+   */
+  private  Analytics initializeAnalytics() throws Exception {
+
+    HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    GoogleCredential credential = GoogleCredential
+        .fromStream(new FileInputStream(KEY_FILE_LOCATION))
+        .createScoped(AnalyticsScopes.all());
+
+    // Construct the Analytics service object.
+    return new Analytics.Builder(httpTransport, JSON_FACTORY, credential)
+        .setApplicationName(APPLICATION_NAME).build();
+  }
+
+
   private static String getFirstProfileId(Analytics analytics) throws IOException {
+    // Get the first view (profile) ID for the authorized user.
     String profileId = null;
-    System.out.println("1.profileId..");
+
+    // Query for the list of all accounts associated with the service account.
     Accounts accounts = analytics.management().accounts().list().execute();
-    System.out.println("2.profileId..");
+
     if (accounts.getItems().isEmpty()) {
       System.err.println("No accounts found");
     } else {
       String firstAccountId = accounts.getItems().get(0).getId();
-      Webproperties properties = analytics.management().webproperties().list("~all").execute();
-      System.out.println("3.profileId..");
+
+      // Query for the list of properties associated with the first account.
+      Webproperties properties = analytics.management().webproperties()
+          .list(firstAccountId).execute();
+
       if (properties.getItems().isEmpty()) {
         System.err.println("No Webproperties found");
       } else {
         String firstWebpropertyId = properties.getItems().get(0).getId();
-        System.out.println("4.profileId..");
-        Profiles profiles = analytics.management().profiles().list(firstAccountId, firstWebpropertyId).execute();
-        System.out.println("5.profileId..");
+
+        // Query for the list views (profiles) associated with the property.
+        Profiles profiles = analytics.management().profiles()
+            .list(firstAccountId, firstWebpropertyId).execute();
+
         if (profiles.getItems().isEmpty()) {
           System.err.println("No views (profiles) found");
         } else {
-          System.out.println("6.profileId..");
+          // Return the first (view) profile associated with the property.
           profileId = profiles.getItems().get(0).getId();
         }
       }
     }
-    System.out.println("10.profileId..END OK!!");
     return profileId;
   }
 
   private static GaData getResults(Analytics analytics, String profileId) throws IOException {
-    return analytics.data().ga().get("ga:" + profileId, "7daysAgo", "today", "ga:sessions").execute();
+    // Query the Core Reporting API for the number of sessions
+    // in the past seven days.
+    return analytics.data().ga()
+        .get("ga:" + profileId, "7daysAgo", "today", "ga:sessions")
+        .execute();
   }
 
   private static void printResults(GaData results) {
-    System.out.println("printResults().results:= " + results);
-    System.out.println("printResults().results.getRows():= " + results.getRows());
+    // Parse the response from the Core Reporting API for
+    // the profile name and number of sessions.
     if (results != null && !results.getRows().isEmpty()) {
-      System.out.println("View (Profile) Name: " + results.getProfileInfo().getProfileName());
+      System.out.println("View (Profile) Name: "
+        + results.getProfileInfo().getProfileName());
       System.out.println("Total Sessions: " + results.getRows().get(0).get(0));
     } else {
       System.out.println("No results found");
