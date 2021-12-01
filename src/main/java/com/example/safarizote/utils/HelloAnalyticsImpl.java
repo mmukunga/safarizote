3989e836f47119b4f3d460b14ace9445d45b006d
Simon Mukunga <mmukunga@gmail.com>
2021-12-01 11:07:49 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/HelloAnalyticsImpl.java
@@ -1,122 +1,155 @@
 package com.example.safarizote.utils;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.analyticsreporting.v4.AnalyticsReporting;
+import com.google.api.services.analyticsreporting.v4.AnalyticsReportingScopes;
+import com.google.api.services.analyticsreporting.v4.model.ColumnHeader;
+import com.google.api.services.analyticsreporting.v4.model.DateRange;
+import com.google.api.services.analyticsreporting.v4.model.DateRangeValues;
+import com.google.api.services.analyticsreporting.v4.model.Dimension;
+import com.google.api.services.analyticsreporting.v4.model.GetReportsRequest;
+import com.google.api.services.analyticsreporting.v4.model.GetReportsResponse;
+import com.google.api.services.analyticsreporting.v4.model.Metric;
+import com.google.api.services.analyticsreporting.v4.model.MetricHeaderEntry;
+import com.google.api.services.analyticsreporting.v4.model.Report;
+import com.google.api.services.analyticsreporting.v4.model.ReportRequest;
+import com.google.api.services.analyticsreporting.v4.model.ReportRow;
 
-import com.google.api.services.analytics.Analytics;
-import com.google.api.services.analytics.AnalyticsScopes;
-import com.google.api.services.analytics.model.Accounts;
-import com.google.api.services.analytics.model.GaData;
-import com.google.api.services.analytics.model.Profiles;
-import com.google.api.services.analytics.model.Webproperties;
-
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-
-/**
- * A simple example of how to access the Google Analytics API using a service
- * account.
- */
 public class HelloAnalyticsImpl {
+  private static final String CLIENT_SECRET_JSON_RESOURCE = "gcmajimoto-958d87dbada8.json";
 
+  // Replace with your view ID.
+  private static final String VIEW_ID = "256217453";
+
+  // The directory where the user's credentials will be stored.
+  private static final File DATA_STORE_DIR = new File(
+      System.getProperty("java.io.tmpdir"), "/hello_analytics");
 
   private static final String APPLICATION_NAME = "GaMajiMoto";
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  private static final String KEY_FILE_LOCATION = "gcmajimoto-958d87dbada8.json";
+  private static NetHttpTransport httpTransport;
+  private static FileDataStoreFactory dataStoreFactory;
 
-  public GaData getGAData() throws Exception {
+  public GetReportsResponse getData() {
     try {
-      Analytics analytics = initializeAnalytics();
+      AnalyticsReporting service = initializeAnalyticsReporting();
+      GetReportsResponse response = getReport(service);
+      printResponse(response);     
+      return response;
 
-      String profile = getFirstProfileId(analytics);
-      System.out.println("First Profile Id: "+ profile);
-      printResults(getResults(analytics, profile));
-      return getResults(analytics, profile);
     } catch (Exception e) {
       e.printStackTrace();
     }
-
     return null;
   }
 
-  /**
-   * Initializes an Analytics service object.
-   *
-   * @return An authorized Analytics service object.
-   * @throws IOException
-   * @throws GeneralSecurityException
-   */
-  private  Analytics initializeAnalytics() throws Exception {
 
-    HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    GoogleCredential credential = GoogleCredential
-        .fromStream(new FileInputStream(KEY_FILE_LOCATION))
-        .createScoped(AnalyticsScopes.all());
+  private static AnalyticsReporting initializeAnalyticsReporting() throws GeneralSecurityException, IOException {
 
-    // Construct the Analytics service object.
-    return new Analytics.Builder(httpTransport, JSON_FACTORY, credential)
+    httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
+
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+        new InputStreamReader(HelloAnalyticsImpl.class
+            .getResourceAsStream(CLIENT_SECRET_JSON_RESOURCE)));
+
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
+        .Builder(httpTransport, JSON_FACTORY, clientSecrets,
+            AnalyticsReportingScopes.all()).setDataStoreFactory(dataStoreFactory)
+        .build();
+
+    Credential credential = new AuthorizationCodeInstalledApp(flow,
+        new LocalServerReceiver()).authorize("user");
+    return new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
         .setApplicationName(APPLICATION_NAME).build();
   }
 
+  private static GetReportsResponse getReport(AnalyticsReporting service) throws IOException {
+    // Create the DateRange object.
+    DateRange dateRange = new DateRange();
+    dateRange.setStartDate("7DaysAgo");
+    dateRange.setEndDate("today");
 
-  private static String getFirstProfileId(Analytics analytics) throws IOException {
-    // Get the first view (profile) ID for the authorized user.
-    String profileId = null;
+    // Create the Metrics object.
+    Metric sessions = new Metric()
+        .setExpression("ga:sessions")
+        .setAlias("sessions");
 
-    // Query for the list of all accounts associated with the service account.
-    Accounts accounts = analytics.management().accounts().list().execute();
+    //Create the Dimensions object.
+    Dimension browser = new Dimension()
+        .setName("ga:browser");
 
-    if (accounts.getItems().isEmpty()) {
-      System.err.println("No accounts found");
-    } else {
-      String firstAccountId = accounts.getItems().get(0).getId();
+    // Create the ReportRequest object.
+    ReportRequest request = new ReportRequest()
+        .setViewId(VIEW_ID)
+        .setDateRanges(Arrays.asList(dateRange))
+        .setDimensions(Arrays.asList(browser))
+        .setMetrics(Arrays.asList(sessions));
 
-      // Query for the list of properties associated with the first account.
-      Webproperties properties = analytics.management().webproperties()
-          .list(firstAccountId).execute();
+    ArrayList<ReportRequest> requests = new ArrayList<ReportRequest>();
+    requests.add(request);
 
-      if (properties.getItems().isEmpty()) {
-        System.err.println("No Webproperties found");
-      } else {
-        String firstWebpropertyId = properties.getItems().get(0).getId();
+    // Create the GetReportsRequest object.
+    GetReportsRequest getReport = new GetReportsRequest()
+        .setReportRequests(requests);
 
-        // Query for the list views (profiles) associated with the property.
-        Profiles profiles = analytics.management().profiles()
-            .list(firstAccountId, firstWebpropertyId).execute();
+    // Call the batchGet method.
+    GetReportsResponse response = service.reports().batchGet(getReport).execute();
 
-        if (profiles.getItems().isEmpty()) {
-          System.err.println("No views (profiles) found");
-        } else {
-          // Return the first (view) profile associated with the property.
-          profileId = profiles.getItems().get(0).getId();
+    // Return the response.
+    return response;
+  }
+
+  /**
+   * Parses and prints the Analytics Reporting API V4 response.
+   *
+   * @param response the Analytics Reporting API V4 response.
+   */
+  private static void printResponse(GetReportsResponse response) {
+
+    for (Report report: response.getReports()) {
+      ColumnHeader header = report.getColumnHeader();
+      List<String> dimensionHeaders = header.getDimensions();
+      List<MetricHeaderEntry> metricHeaders = header.getMetricHeader().getMetricHeaderEntries();
+      List<ReportRow> rows = report.getData().getRows();
+
+      if (rows == null) {
+         System.out.println("No data found for " + VIEW_ID);
+         return;
+      }
+
+      for (ReportRow row: rows) {
+        List<String> dimensions = row.getDimensions();
+        List<DateRangeValues> metrics = row.getMetrics();
+        for (int i = 0; i < dimensionHeaders.size() && i < dimensions.size(); i++) {
+          System.out.println(dimensionHeaders.get(i) + ": " + dimensions.get(i));
+        }
+
+        for (int j = 0; j < metrics.size(); j++) {
+          System.out.print("Date Range (" + j + "): ");
+          DateRangeValues values = metrics.get(j);
+          for (int k = 0; k < values.getValues().size() && k < metricHeaders.size(); k++) {
+            System.out.println(metricHeaders.get(k).getName() + ": " + values.getValues().get(k));
+          }
         }
       }
-    }
-    return profileId;
-  }
-
-  private static GaData getResults(Analytics analytics, String profileId) throws IOException {
-    // Query the Core Reporting API for the number of sessions
-    // in the past seven days.
-    return analytics.data().ga()
-        .get("ga:" + profileId, "7daysAgo", "today", "ga:sessions")
-        .execute();
-  }
-
-  private static void printResults(GaData results) {
-    // Parse the response from the Core Reporting API for
-    // the profile name and number of sessions.
-    if (results != null && !results.getRows().isEmpty()) {
-      System.out.println("View (Profile) Name: "
-        + results.getProfileInfo().getProfileName());
-      System.out.println("Total Sessions: " + results.getRows().get(0).get(0));
-    } else {
-      System.out.println("No results found");
     }
   }
 }
