5b73f4a4ca9997ffbfec1772b1ba24d50db2d755
Simon Mukunga <mmukunga@gmail.com>
2021-12-03 00:07:11 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/GoogleAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/GoogleAnalyticsImpl.java
@@ -26,6 +26,7 @@ import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Service("gaService")
@@ -74,6 +75,7 @@ public class GoogleAnalyticsImpl implements GoogleAnalytics {
         .setTransport(httpTransport)
         .setJsonFactory(JSON_FACTORY)
         .setClientSecrets(clientId, clientSecret)
+        .setServiceAccountScopes(Collections.singleton("https://www.googleapis.com/auth/analytics.readonly"))
         .build();
 
     AnalyticsReporting  analyticReporting = new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
