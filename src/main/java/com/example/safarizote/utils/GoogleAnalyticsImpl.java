a69af1304520ede3d73fb28b2b5aa169cd6ee99c
Simon Mukunga <mmukunga@gmail.com>
2021-12-02 22:39:48 +0100

    first commit

--- src/main/java/com/example/safarizote/utils/GoogleAnalyticsImpl.java
+++ src/main/java/com/example/safarizote/utils/GoogleAnalyticsImpl.java
@@ -1,20 +1,12 @@
 package com.example.safarizote.utils;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.analyticsreporting.v4.AnalyticsReporting;
-import com.google.api.services.analyticsreporting.v4.AnalyticsReportingScopes;
 import com.google.api.services.analyticsreporting.v4.model.ColumnHeader;
 import com.google.api.services.analyticsreporting.v4.model.DateRange;
 import com.google.api.services.analyticsreporting.v4.model.DateRangeValues;
@@ -29,8 +21,6 @@ import com.google.api.services.analyticsreporting.v4.model.ReportRow;
 
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
@@ -45,14 +35,8 @@ public class GoogleAnalyticsImpl implements GoogleAnalytics {
   // Replace with your view ID.
   private static final String VIEW_ID = "256217453";
 
-  // The directory where the user's credentials will be stored.
-  private static final File DATA_STORE_DIR = new File(
-      System.getProperty("java.io.tmpdir"), "/hello_analytics");
-
   private static final String APPLICATION_NAME = "GaMajiMoto";
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  //private static NetHttpTransport httpTransport;
-  //private static FileDataStoreFactory dataStoreFactory;
 
   public GetReportsResponse getGAData() {
     try {
@@ -71,31 +55,18 @@ public class GoogleAnalyticsImpl implements GoogleAnalytics {
 
 
   private static AnalyticsReporting initializeAnalyticsReporting() throws GeneralSecurityException, IOException {
-
-    //NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    //FileDataStoreFactory dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
-
     System.out.println("Maji");
     java.io.InputStream is = GoogleAnalyticsImpl.class.getResourceAsStream(CLIENT_SECRET_JSON_RESOURCE);
     System.out.println(is);
     System.out.println("1Moto");
-
     GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
         new InputStreamReader(GoogleAnalyticsImpl.class
             .getResourceAsStream(CLIENT_SECRET_JSON_RESOURCE)));
-
+    
+    System.out.println("2Moto");
     String clientId = clientSecrets.get("client_id").toString();
     String clientSecret = clientSecrets.get("private_key").toString();
-    System.out.println("2Moto");
-    /*
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
-        .Builder(httpTransport, JSON_FACTORY, clientId, clientSecret,
-            AnalyticsReportingScopes.all()).setDataStoreFactory(dataStoreFactory)
-        .build();
-    */
-   
     System.out.println("3Moto");
-    //Credential credential0 = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
     System.out.println("4Moto");
 
     HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
@@ -104,12 +75,6 @@ public class GoogleAnalyticsImpl implements GoogleAnalytics {
         .setJsonFactory(JSON_FACTORY)
         .setClientSecrets(clientId, clientSecret)
         .build();
-    
-    /*    
-    GoogleCredential credential1 = GoogleCredential
-        .fromStream(new FileInputStream(CLIENT_SECRET_JSON_RESOURCE))
-        .createScoped(AnalyticsReportingScopes.all());
-    */
 
     AnalyticsReporting  analyticReporting = new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
     .setApplicationName(APPLICATION_NAME).build();
