470ee3834f09b8e7e7dee5f3fb3c8f9715823307
Simon Mukunga <mmukunga@gmail.com>
2022-08-06 22:28:50 +0200

    first commit

--- src/main/java/com/safari/springboot/safarizote/repository/AnalyticsRepository.java
+++ src/main/java/com/safari/springboot/safarizote/repository/AnalyticsRepository.java
@@ -5,9 +5,12 @@ import com.safari.springboot.safarizote.model.Analytics;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnalyticsRepository extends JpaRepository<Analytics, Long>{
-   List<Analytics> findByIPv4(String iPv4);
+   @Query(value = "SELECT max(id) FROM analytics")
+    public Long getMaxId();
+    public List<Analytics> findByIPv4(String iPv4);
 }
