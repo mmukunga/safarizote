fa6b412982f9937c099ad4ef2e918541efce24e2
Simon Mukunga <mmukunga@gmail.com>
2022-08-07 01:44:46 +0200

    first commit

--- src/main/java/com/safari/springboot/safarizote/model/Analytics.java
+++ src/main/java/com/safari/springboot/safarizote/model/Analytics.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,7 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-@Table(name="analytics")
+@Table(name="analytics",uniqueConstraints=@UniqueConstraint(columnNames={"ipv4"}))
 public class Analytics {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
